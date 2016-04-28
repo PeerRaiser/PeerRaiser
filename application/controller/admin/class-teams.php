055df21598a14fccaea3ed73bbf37a28f6017889
Nathan Allen <nathan@fireflypartners.com>
2016-04-28 19:47:19 -0400

    Added Teams Controller

---
+++ application/controller/admin/class-teams.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PeerRaiser\Controller\Admin;
+
+class Teams extends Base {
+
+    private static $instance = null;
+
+    /**
+     * @see \PeerRaiser\Core\Event\SubscriberInterface::get_subscribed_events()
+     */
+    public static function get_subscribed_events() {
+        return array(
+            'peerraiser_cmb2_admin_init' => array(
+                array( 'peerraiser_on_admin_view', 200 ),
+                array( 'peerraiser_on_plugin_is_active', 200 ),
+                array( 'register_meta_boxes' )
+            ),
+            'peerraiser_admin_enqueue_styles_post_new' => array(
+                array( 'peerraiser_on_admin_view', 200 ),
+                array( 'peerraiser_on_plugin_is_active', 200 ),
+                array( 'load_assets' )
+            ),
+            'peerraiser_admin_enqueue_styles_post_edit' => array(
+                array( 'peerraiser_on_admin_view', 200 ),
+                array( 'peerraiser_on_plugin_is_active', 200 ),
+                array( 'load_assets' )
+            ),
+        );
+    }
+
+
+    /**
+     * Singleton to get only one Campaigns controller
+     *
+     * @return    \PeerRaiser\Admin\Campaigns
+     */
+    public static function get_instance() {
+        if ( ! isset( self::$instance ) ) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+
+    public function __construct(){}
+
+
+    public function register_meta_boxes( \PeerRaiser\Core\Event $event ) {
+
+        $teams_model = \PeerRaiser\Model\Admin\Teams::get_instance();
+        $team_field_groups = $teams_model->get_fields();
+
+        foreach ($team_field_groups as $field_group) {
+            $cmb = new_cmb2_box( array(
+                'id'           => $field_group['id'],
+                'title'         => $field_group['title'],
+                'object_types'  => array( 'pr_team' ),
+                'context'       => $field_group['context'],
+                'priority'      => $field_group['priority'],
+            ) );
+            foreach ($field_group['fields'] as $key => $value) {
+                $cmb->add_field($value);
+            }
+        }
+
+    }
+
+
+    public function load_assets() {
+        parent::load_assets();
+
+        // If this isn't the Fundraiser post type, exit early
+        global $post_type;
+        if ( 'pr_team' != $post_type )
+            return;
+
+        // Register and enqueue styles
+        wp_register_style(
+            'peerraiser-admin',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin.css',
+            array('peerraiser-font-awesome', 'peerraiser-select2'),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version')
+        );
+        wp_register_style(
+            'peerraiser-admin-teams',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin-teams.css',
+            array('peerraiser-font-awesome', 'peerraiser-admin', 'peerraiser-select2'),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version')
+        );
+        wp_enqueue_style( 'peerraiser-admin' );
+        wp_enqueue_style( 'peerraiser-admin-teams' );
+        wp_enqueue_style( 'peerraiser-font-awesome' );
+        wp_enqueue_style( 'peerraiser-select2' );
+
+        // Register and enqueue scripts
+        wp_register_script(
+            'peerraiser-admin-teams',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('js_url') . 'peerraiser-admin-teams.js',
+            array( 'jquery', 'peerraiser-admin', 'peerraiser-select2' ),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version'),
+            true
+        );
+        wp_enqueue_script( 'peerraiser-admin' ); // Already registered in Admin class
+        wp_enqueue_script( 'peerraiser-admin-teams' );
+        wp_enqueue_script( 'peerraiser-select2' );
+
+        // Localize scripts
+        wp_localize_script(
+            'peerraiser-admin-teams',
+            'peerraiser_object',
+            array(
+                'ajax_url' => admin_url( 'admin-ajax.php' ),
+                'template_directory' => get_template_directory_uri()
+            )
+        );
+
+    }
+
+ }
