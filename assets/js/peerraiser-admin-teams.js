32435977a8b6d539cf88629d685eb1f07494c651
Nate Allen <email@ncallen.com>
2016-04-28 19:48:27 -0400

    Added JS for Teams Admin page

---
+++ assets/js/peerraiser-admin-teams.js
@@ -0,0 +1,86 @@
+(function( $ ) {$(function() {
+    'use strict';
+
+    function peerRaiserAdminTeam(){
+        var $o = {
+            dashboardTab           : $('#toplevel_page_peerraiser-dashboard'),
+            dasboardTabLink        : $('#toplevel_page_peerraiser-dashboard > a'),
+            fundraiserLink         : $('#toplevel_page_peerraiser-dashboard a[href$="pr_team"]'),
+
+            select2Fields          : {
+                campaign           : $("#_team_campaign"),
+                fundraisers        : $("#_team_fundraisers"),
+            },
+
+            select2Options         : {
+                campaign           : {
+                    data           : function ( params ) {
+                        return {
+                            action: 'peerraiser_get_posts',
+                            s: params.term,
+                            page: params.page,
+                            post_type  : ['pr_campaign']
+                        };
+                    },
+                    templateResult : function(data) {
+                        var html = '<span class="pr_name">' + data.text + '</span>';
+                        if ( data.id ) {
+                            html += '<span class="pr_id">Campaign ID: ' + data.id + '</span>';
+                        }
+                        return $('<span>').html(html);
+                    }
+                },
+                fundraisers : {
+                    data : function (params) {
+                        return {
+                            action: 'peerraiser_get_posts',
+                            q: params.term,
+                            page: params.page,
+                            post_type  : ['fundraiser']
+                        };
+                    },
+                    templateResult: function(data) {
+                        var html = '<span class="display_name">' + data.text + '</span>';
+                        if ( data.id ) {
+                            html += '<span class="user_id">Fundraiser ID: ' + data.id + '</span>';
+                        }
+                        return $('<span>').html(html);
+                    },
+                    multiple: true,
+                },
+            },
+        },
+
+        init = function(){
+            bindEvents();
+            renderSelect();
+            activateSubmenu();
+        },
+
+        bindEvents = function() {
+
+        },
+
+        renderSelect = function() {
+            for ( var key in $o.select2Fields ) {
+                if ( $o.select2Fields[key].length ){
+                    $o.select2Fields[key].renderSelect($o.select2Options[key]);
+                }
+            }
+        },
+
+        // WordPress doesn't display submenus correctly if they're a post type. This is the workaround...
+        activateSubmenu= function() {
+            $o.dashboardTab.removeClass('wp-not-current-submenu').addClass('wp-has-current-submenu');
+            $o.dasboardTabLink.addClass('wp-has-current-submenu').removeClass('wp-not-current-submenu');
+            $o.fundraiserLink.addClass('current').parent().addClass('current');
+        };
+
+        init();
+
+    }
+
+    // Kick it off
+    peerRaiserAdminTeam();
+
+});})(jQuery);
