42fc9127dc3524e94becb70101d4f4d043c9231b
Nate Allen <email@ncallen.com>
2016-04-28 20:05:08 -0400

    Removed unused reports/settings templates

--- views/backend/reports-page-template.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap peerraiser" id="peerraiser-settings">
-
-    <h2><?php _e('Reports', 'peerraiser') ?></h2>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, deserunt repudiandae aliquid amet, voluptates dolorum et aut similique magni dolor ratione cumque eaque at quis nostrum incidunt repellat. Eaque, cupiditate!</p>
-
-</div>
