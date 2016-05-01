d71b79f1c4c364c49ce5a44ab18e9d9fef4cac77
Nate Allen <email@ncallen.com>
2016-05-01 01:28:53 -0400

    Added tabs to Settings page

--- views/backend/settings.php
+++ views/backend/settings.php
@@ -9,6 +9,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <h2><?php _e('PeerRaiser Settings', 'peerraiser') ?></h2>
 
+    <h2 class="nav-tab-wrapper">
+        <?php foreach( $peerraiser['tabs'] as $id => $name ) : ?>
+        <a href="<?php echo esc_url( add_query_arg( array( 'tab' => $id ), admin_url( 'admin.php?page=peerraiser-settings' ) ) ) ?>" class="nav-tab<?php echo $peerraiser['active_tab'] == $id ? ' nav-tab-active' : '' ?>"><?= $name ?></a>
+        <?php endforeach; ?>
+    </h2>
+
     <?= $peerraiser['fields'] ?>
 
 </div>
