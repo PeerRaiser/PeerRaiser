9c0a72d106b0b513fc828f3d4c5dc597f4cf8431
Nathan Allen <nathan@fireflypartners.com>
2016-05-11 13:24:44 -0400

    Add translation functions

--- views/backend/dashboard.php
+++ views/backend/dashboard.php
@@ -13,21 +13,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <div class="wrap peerraiser-wrap dashboard-wrap">
 
-        <h1><strong>PeerRaiser</strong><sup class="version"><?= $peerraiser['plugin_version'] ?></sup></h1>
+        <h1><strong><?php _e('PeerRaiser', 'peerraiser') ?></strong><sup class="version"><?= $peerraiser['plugin_version'] ?></sup></h1>
 
         <div class="column column-left">
 
             <?php if ( $peerraiser['show_welcome_message'] ) : ?>
                 <div class="welcome-message">
-                    <h2>Welcome to your dashboard, <?= $peerraiser['display_name'] ?></h2>
-                    <p>The dashboard provides an overview of your peer-to-peer campaigns, fundraising tips, and the latest news about this plugin.</p>
+                    <h2><?php printf( esc_html__( 'Welcome to your dashboard, $s', 'peerraiser' ), $peerraiser['display_name'] ); ?></h2>
+                    <p><?php _e('The dashboard provides an overview of your peer-to-peer campaigns, fundraising tips, and the latest news about this plugin.', 'peerraiser') ?></p>
 
-                    <h3>Let's get you started...</h3>
+                    <h3><?php _e("Let's get you started...", 'peerraiser') ?></h3>
 
                     <ul>
-                        <li class="status-complete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_1'] ?>" aria-hidden="true"></i><a href="https://peerraiser.com/join">Create a free PeerRaiser account</a></li>
-                        <li class="status-incomplete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_2'] ?>" aria-hidden="true"></i><a href="<?= $peerraiser['admin_url'] ?>admin.php?page=peerraiser-settings">Configure your settings</a></li>
-                        <li class="status-incomplete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_3'] ?>" aria-hidden="true"></i><a href="<?= $peerraiser['admin_url'] ?>post-new.php?post_type=pr_campaign">Create your first campaign</a></li>
+                        <li class="status-complete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_1'] ?>" aria-hidden="true"></i><a href="https://peerraiser.com/join"><?php _e('Create a free PeerRaiser account', 'peerraiser') ?></a></li>
+                        <li class="status-incomplete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_2'] ?>" aria-hidden="true"></i><a href="<?= $peerraiser['admin_url'] ?>admin.php?page=peerraiser-settings"><?php _e('Configure your settings', 'peerraiser') ?></a></li>
+                        <li class="status-incomplete"><i class="fa fa-fw <?= $peerraiser['font_awesome_class']['step_3'] ?>" aria-hidden="true"></i><a href="<?= $peerraiser['admin_url'] ?>post-new.php?post_type=pr_campaign"><?php _e('Create your first campaign', 'peerraiser') ?></a></li>
                     </ul>
 
                     <a href="#" class="close" data-message-type="welcome_message" data-nonce="<?= wp_create_nonce("dismiss_welcome_message") ?>"><i class="fa fa-times-circle" aria-hidden="true"></i></a>
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
         </div>
         <div class="column column-right">
-            <h2>Latest News</h2>
+            <h2><?php _e('Activity Feed', 'peerraiser') ?></h2>
             <p>Something cool happened today</p>
         </div>
 
