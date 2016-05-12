bc5e174bcbb0ab47a1c3512686d7786848c7c12f
Nathan Allen <nathan@fireflypartners.com>
2016-05-11 22:34:44 -0400

    Create 'Donors' controller class

---
+++ application/controller/admin/class-donors.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PeerRaiser\Controller\Admin;
+
+class Donors extends \PeerRaiser\Controller\Base {
+
+    private static $instance = null;
+
+    /**
+     * @see \PeerRaiser\Core\Event\SubscriberInterface::get_subscribed_events()
+     */
+    public static function get_subscribed_events() {
+        return array(
+            'peerraiser_admin_enqueue_styles_post_new' => array(
+                array( 'peerraiser_on_admin_view', 200 ),
+                array( 'peerraiser_on_plugin_is_active', 200 ),
+                array( 'load_assets' )
+            ),
+            'peerraiser_admin_enqueue_styles_post_edit' => array(
+                array( 'peerraiser_on_admin_view', 200 ),
+                array( 'peerraiser_on_plugin_is_active', 200 ),
+                array( 'load_assets' )
+            ),
+        );
+    }
+
+
+    public function load_assets() {
+        parent::load_assets();
+
+        // If this isn't the Donor post type, exit early
+        global $post_type;
+        if ( 'pr_donor' != $post_type )
+            return;
+
+        // Register and enqueue styles
+        wp_register_style(
+            'peerraiser-admin',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin.css',
+            array('peerraiser-font-awesome'),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version')
+        );
+        wp_register_style(
+            'peerraiser-admin-donors',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin-donors.css',
+            array('peerraiser-font-awesome', 'peerraiser-admin'),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version')
+        );
+        wp_enqueue_style( 'peerraiser-admin' );
+        wp_enqueue_style( 'peerraiser-admin-donors' );
+        wp_enqueue_style( 'peerraiser-font-awesome' );
+        wp_enqueue_style( 'peerraiser-select2' );
+
+        // Register and enqueue scripts
+        wp_register_script(
+            'peerraiser-admin-donors',
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('js_url') . 'peerraiser-admin-donors.js',
+            array( 'jquery', 'peerraiser-admin' ),
+            \PeerRaiser\Core\Setup::get_plugin_config()->get('version'),
+            true
+        );
+        wp_enqueue_script( 'peerraiser-admin' ); // Already registered in Admin class
+        wp_enqueue_script( 'peerraiser-admin-donors' );
+        wp_enqueue_script( 'peerraiser-select2' );
+
+        // Localize scripts
+        wp_localize_script(
+            'peerraiser-admin-donors',
+            'peerraiser_object',
+            array(
+                'ajax_url' => admin_url( 'admin-ajax.php' ),
+                'template_directory' => get_template_directory_uri()
+            )
+        );
+
+    }
+
+}
