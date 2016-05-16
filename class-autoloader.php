27702cf3a5bc7d7cd87fa8f06ad1fbc76a630107
Nate Allen <email@ncallen.com>
2016-05-16 17:01:35 -0400

    Add copyright info

--- class-autoloader.php
+++ class-autoloader.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * (c) PeerRaiser <http://peerraiser.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace PeerRaiser;
 
