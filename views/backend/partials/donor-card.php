3473c7d39e3c94aa33dd94788b08a6deba734baf
Nate Allen <email@ncallen.com>
2016-05-24 00:38:21 -0400

    Add HTML for Donor card

---
+++ views/backend/partials/donor-card.php
@@ -0,0 +1,11 @@
+<div id="donor-card">
+    <img src="<?= $peerraiser['profile_image_url'] ?>" alt="Profile Picture" class="<?= $peerraiser['donor_class'] ?> profile-image">
+    <div class="donor-info">
+        <h1><?= $peerraiser['first_name'] ?> <?= $peerraiser['last_name'] ?> <span>#<?= $peerraiser['donor_id'] ?></span></h1>
+        <div class="donor-meta">
+            <p class="email"><?= $peerraiser['donor_email'] ?></p>
+            <p class="since">Donor since <?= $peerraiser['donor_since'] ?></p>
+            <p class="user-account">User Account: <?= $peerraiser['donor_user_account'] ?></p>
+        </div>
+    </div>
+</div>
