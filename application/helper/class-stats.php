2f0c137acfa83cccd48cab0f3b4ba9b54246b755
Nathan Allen <nathan@fireflypartners.com>
2016-05-24 00:36:54 -0400

    Create Stats helper class

---
+++ application/helper/class-stats.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace PeerRaiser\Helper;
+
+/**
+ * PeerRaiser donation helper.
+ */
+class Stats {
+
+    public static function get_total_donations(){
+        global $wpdb;
+
+        $total = get_transient( 'peerraiser_donations_total' );
+
+        if ( false === $total ) {
+
+            $total = (float) 0;
+
+            $args = array(
+                'post_type'      => 'pr_donation',
+                'posts_per_page' => -1,
+                'post_status'    => 'publish',
+            );
+            $donations = new \WP_Query( $args );
+
+            $post_ids = wp_list_pluck( $donations->posts, 'ID' );
+
+            if ( $post_ids ) {
+                $post_ids = implode( ',', $post_ids );
+                $total += $wpdb->get_var( "SELECT SUM(meta_value) FROM $wpdb->postmeta WHERE meta_key = '_donation_amount' AND post_id IN({$post_ids})" );
+            }
+
+            // Cache results for 1 day. This cache is cleared automatically when a payment is made
+            set_transient( 'peerraiser_donations_total', $total, 86400 );
+
+        }
+
+        if( $total < 0 ) {
+            $total = 0;
+        }
+
+        return number_format_i18n( $total, 2 );
+    }
+
+    public static function get_total_donations_by_fundraiser( $fundraiser_id ) {
+        global $wpdb;
+
+        $total = (float) 0;
+
+        $args = array(
+            'post_type'       => 'pr_donation',
+            'posts_per_page'  => -1,
+            'post_status'     => 'publish',
+            'connected_type'  => 'donation_to_fundraiser',
+            'connected_items' => $fundraiser_id
+        );
+        $donations = new \WP_Query( $args );
+
+        $post_ids = wp_list_pluck( $donations->posts, 'ID' );
+
+        if ( $post_ids ) {
+            $post_ids = implode( ',', $post_ids );
+            $total += $wpdb->get_var( "SELECT SUM(meta_value) FROM $wpdb->postmeta WHERE meta_key = '_donation_amount' AND post_id IN({$post_ids})" );
+        }
+
+        return number_format_i18n( $total, 2);
+
+    }
+
+    public static function get_top_donors( $limit = 10 ){
+        global $wpdb;
+
+        return $wpdb->get_results("
+        SELECT SUM(meta_value) as total, p2p_to as ID FROM $wpdb->postmeta AS m
+        INNER JOIN $wpdb->posts AS p
+           ON m.post_id = p.ID
+        INNER JOIN {$wpdb->prefix}p2p AS r
+           ON m.post_id = r.p2p_from
+        WHERE m.meta_key = '_donation_amount'
+        AND p.post_type = 'pr_donation'
+        AND r.p2p_type = 'donation_to_donor'
+        AND p.post_status = 'publish'
+        GROUP BY r.p2p_to
+        ORDER BY SUM(meta_value) DESC
+        LIMIT $limit
+        ");
+
+    }
+
+    public static function get_top_fundraisers( $limit = 10 ){
+        global $wpdb;
+
+        return $wpdb->get_results("
+        SELECT SUM(meta_value) as total, p2p_to as ID FROM $wpdb->postmeta AS m
+        INNER JOIN $wpdb->posts AS p
+           ON m.post_id = p.ID
+        INNER JOIN {$wpdb->prefix}p2p AS r
+           ON m.post_id = r.p2p_from
+        WHERE m.meta_key = '_donation_amount'
+        AND p.post_type = 'pr_donation'
+        AND r.p2p_type = 'donation_to_fundraiser'
+        AND p.post_status = 'publish'
+        GROUP BY r.p2p_to
+        ORDER BY SUM(meta_value) DESC
+        LIMIT $limit
+        ");
+
+    }
+}
