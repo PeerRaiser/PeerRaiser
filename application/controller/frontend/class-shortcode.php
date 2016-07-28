e4ba257aca83134516b41fcfc1d1672650b5a1bd
Nathan Allen <nathan@fireflypartners.com>
2016-07-28 09:25:04 -0400

    Add render_login_form and render_signup_form functions

--- application/controller/frontend/class-shortcode.php
+++ application/controller/frontend/class-shortcode.php
@@ -34,18 +34,78 @@ class Shortcode extends \PeerRaiser\Controller\Base {
 
         // provide default values for empty shortcode attributes
         $a = shortcode_atts( array(
-            'target_post_id'    => '',
-            'target_post_title' => '',
-            'heading_text'      => '',
-            'description_text'  => '',
-            'content_type'      => '',
-            'teaser_image_path' => '',
+            'heading_text'     => '',
+            'description_text' => '',
         ), $atts );
 
         $html = '<p><strong>Testing</strong> 123</p>';
 
         $event->set_result( $html );
 
+    }
+
+
+    public function render_login_form( \PeerRaiser\Core\Event $event ) {
+        list( $atts ) = $event->get_arguments() + array( array() );
+
+        // provide default values for empty shortcode attributes
+        $a = shortcode_atts( array(
+            'heading_text'     => '',
+            'description_text' => '',
+        ), $atts );
+
+        $view_args = array(
+            'test' => 'test result'
+        );
+        $this->assign( 'peerraiser', $view_args );
+
+        $event->set_result( $this->get_text_view( 'frontend/partials/login-form' ) );
+
+    }
+
+
+    public function render_signup_form( \PeerRaiser\Core\Event $event ) {
+        list( $atts ) = $event->get_arguments() + array( array() );
+
+        // provide default values for empty shortcode attributes
+        $a = shortcode_atts( array(
+            'heading_text'     => '',
+            'description_text' => '',
+        ), $atts );
+
+        $view_args = array(
+            'test' => 'test result'
+        );
+        $this->assign( 'peerraiser', $view_args );
+
+        $event->set_result( $this->get_text_view( 'frontend/partials/signup-form' ) );
+
+    }
+
+
+    public function render_participant_dashboard( \PeerRaiser\Core\Event $event ) {
+        list( $atts ) = $event->get_arguments() + array( array() );
+
+        // provide default values for empty shortcode attributes
+        $a = shortcode_atts( array(
+            'heading_text'     => '',
+            'description_text' => '',
+        ), $atts );
+
+        // Get the default dashboard and login page urls
+        $plugin_options        = get_option( 'peerraiser_options', array() );
+        $participant_dashboard = get_permalink( $plugin_options[ 'participant_dashboard' ] );
+        $login_page            = get_permalink( $plugin_options[ 'login_page' ] );
+
+        // If the user isn't logged in, redirect to the login page
+        if ( !is_user_logged_in() ) {
+            $args = array(
+                'next_url' => $participant_dashboard
+            );
+
+            wp_safe_redirect( add_query_arg( $args, $login_page ) );
+            exit;
+        }
     }
 
 }
