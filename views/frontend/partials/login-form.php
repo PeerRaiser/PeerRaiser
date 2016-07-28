ae52d100b974ca3d75716b64eb2eb90f3385ad93
Nate Allen <email@ncallen.com>
2016-07-28 09:29:02 -0400

    Add error messages

--- views/frontend/partials/login-form.php
+++ views/frontend/partials/login-form.php
@@ -1,40 +1,50 @@
 <div class="login-form-wrapper">
-    <div class="login-form-heading"><h3 class="login-title"><strong><?php __( 'Sign in', 'peerraiser' ) ?> </strong></h3>
-        <div class="forgot-password"><a href="#"><?php __( 'Forgot password?', 'peerraiser' ) ?></a></div>
+    <div class="login-form-heading"><h3 class="login-title"><?php _e( 'Sign in', 'peerraiser' ) ?></h3>
+        <div class="forgot-password"><a href="#"><?php _e( 'Forgot password?', 'peerraiser' ) ?></a></div>
     </div>
 
     <div class="login-form-body">
-        <form role="form">
-            <div class="error-message">
-                <a class="close" href="#">×</a><?php __( 'Incorrect Username or Password!', 'peerraiser' ) ?>
-            </div>
+        <form role="form" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+            <?php if ( isset( $_GET['errors'] ) ) : ?>
+                <div class="error-message">
+                    <?php if ( $_GET['errors'] === 'login' ) : ?>
+                        <?php _e( '<strong>Oops!</strong> Incorrect Username or Password.', 'peerraiser' ) ?>
+                    <?php elseif ( $_GET['errors'] === 'empty_fields' ) : ?>
+                        <?php _e( '<strong>Oops!</strong> Username or Password was empty.', 'peerraiser' ) ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
 
             <div class="input-wrapper email">
-                <input id="login-email" type="email" name="email" value="" placeholder="<?php __( 'email address', 'peerraiser' ) ?>">
+                <label for="login-email"><?php _e( 'Username or Email Address', 'peerraiser' ) ?>
+                    <input id="login-email" class="form-input" type="text" name="username" value="" placeholder="">
+                </label>
             </div>
 
             <div class="input-wrapper password">
-                <input id="login-password" type="password" name="password" placeholder="<?php __( 'password', 'peerraiser' ) ?>">
+                <label for="login-password"><?php _e( 'Password', 'peerraiser' ) ?>
+                    <input id="login-password" class="form-input" type="password" name="password">
+                </label>
             </div>
 
             <div class="input-wrapper">
                 <div class="checkbox">
                     <label>
-                        <input id="login-remember" type="checkbox" name="remember" value="1"> <?php __( 'Remember me', 'peerraiser' ) ?>
+                        <input id="login-remember" type="checkbox" name="remember" value="1"> <?php _e( 'Remember me', 'peerraiser' ) ?>
                     </label>
                 </div>
             </div>
 
-            <button type="submit" class="login-submit"><?php __( 'Sign in', 'peerraiser' ) ?></button>
+            <button type="submit" class="login-submit"><?php _e( 'Sign in', 'peerraiser' ) ?></button>
 
-            <hr>
-
-            <div>
-                Don't have an account!
-                <a href="#" onclick="$('#loginbox').hide(); $('#signupbox').show()">
-                    Sign Up Here
-                </a>
+            <div class="login-form-bottom">
+                <?php _e( 'Don\'t have an account?', 'peerraiser') ?> <a href="#"><?php _e( 'Sign Up Here', 'peerraiser' ) ?></a>
             </div>
+
+            <input type="hidden" name="action" value="peerraiser_login">
+            <?php if ( isset( $_GET['next_url'] ) ) : ?>
+                <input type="hidden" name="next_url" value="<?= esc_url( $_GET['next_url'] ) ?>">
+            <?php endif; ?>
 
         </form>
     </div>
