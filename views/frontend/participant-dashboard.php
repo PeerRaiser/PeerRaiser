223b1bd6194ca9facee10ebab4542ef3991e19cf
Nate Allen <email@ncallen.com>
2016-07-28 09:36:30 -0400

    Add participant dashboard HTML

---
+++ views/frontend/participant-dashboard.php
@@ -0,0 +1,7 @@
+<nav class="peerraiser-nav">
+    <ul>
+        <li><a href="#">My Profile</a></li>
+        <li><a href="#">Donations</a></li>
+        <li><a href="#">Settings</a></li>
+    </ul>
+</nav>
