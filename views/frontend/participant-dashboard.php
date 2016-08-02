6876d07576a2171925d735aadd64c72f722c819a
Nate Allen <email@ncallen.com>
2016-08-01 21:58:05 -0400

    Removed participant dashboard file

--- views/frontend/participant-dashboard.php
+++
@@ -1,7 +0,0 @@
-<nav class="peerraiser-nav">
-    <ul>
-        <li><a href="#">My Profile</a></li>
-        <li><a href="#">Donations</a></li>
-        <li><a href="#">Settings</a></li>
-    </ul>
-</nav>
