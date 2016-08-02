b4a28c925bdc3f299002640c18ed4ec8392629c3
Nathan Allen <nathan@fireflypartners.com>
2016-08-01 21:54:34 -0400

    Add functions to get donations, fundraisers, and teams for the dashboard

--- application/model/frontend/class-dashboard.php
+++ application/model/frontend/class-dashboard.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace PeerRaiser\Model\Admin;
+namespace PeerRaiser\Model\Frontend;
 
 class Dashboard extends \PeerRaiser\Model\Admin {
 
-    private static $fields     = array();
-    private static $navigation = null;
+    private static $instance   = null;
+    private static $navigation = array();
 
     public function __construct() {}
 
@@ -16,10 +16,62 @@ class Dashboard extends \PeerRaiser\Model\Admin {
      */
     public static function get_instance() {
         if ( ! isset( self::$instance ) ) {
-            self::$instance = new self();
-            self::$navigation = array();
+            self::$instance   = new self();
+            self::$navigation = array(
+                "profile"   => __( "My Profile", 'peerraiser' ),
+                "donations" => __( "Donations", 'peerraiser' ),
+                "settings"  => __( "Settings", 'peerraiser' ),
+            );
         }
         return self::$instance;
+    }
+
+
+    public function get_navigation() {
+        return self::$navigation;
+    }
+
+
+    public function get_donations( $post_id = null, $paged = 1 ) {
+        $args = array(
+            'post_type'       => 'pr_donation',
+            'posts_per_page'  => 20,
+            'post_status'     => 'publish',
+            'connected_type'  => 'donation_to_donor',
+            'connected_items' => $post_id,
+            'paged' => $paged
+        );
+        return new \WP_Query( $args );
+    }
+
+
+    public function get_fundraisers() {
+        $args = array(
+            'post_type'       => 'fundraiser',
+            'posts_per_page'  => -1,
+            'post_status'     => 'publish',
+            'connected_type'  => 'fundraiser_to_participant',
+            'connected_items' => get_current_user_id(),
+        );
+        return new \WP_Query( $args );
+    }
+
+
+    /**
+     * Get teams where the current user is the leader
+     *
+     * @since     1.0.0
+     * @return    WP_Query
+     */
+    public function get_teams() {
+        $args = array(
+            'post_type' => 'pr_team',
+            'posts_per_page' => -1,
+            'post_status' => 'publish',
+            'connected_type' => 'teams_to_captains',
+            'connected_items' => get_current_user_id(),
+        );
+        return new \WP_Query( $args );
     }
 
 }
