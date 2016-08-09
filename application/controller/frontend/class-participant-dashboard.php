2b72e8d1c6802c30c64d1431c755da85e820568d
Nate Allen <email@ncallen.com>
2016-08-09 00:03:00 -0400

    Add function to update avatar via AJAX

--- application/controller/frontend/class-participant-dashboard.php
+++ application/controller/frontend/class-participant-dashboard.php
@@ -4,4 +4,38 @@ namespace PeerRaiser\Controller\Frontend;
 
 class Participant_Dashboard extends \PeerRaiser\Controller\Base {
 
+    public static function get_subscribed_events() {
+        return array(
+            'wp_ajax_peerraiser_update_avatar' => array(
+                array( 'ajax_update_avatar', 100 ),
+                array( 'peerraiser_on_plugin_is_working', 200 ),
+                array( 'peerraiser_on_ajax_send_json', 300 ),
+            ),
+        );
+    }
+
+    public function ajax_update_avatar( \PeerRaiser\Core\Event $event ) {
+        $user_id = get_current_user_id();
+
+        $avatar_id = get_user_meta( $user_id, '_peerraiser_custom_avatar', true );
+
+        /* If the avatar has been set previously, remove it from the media library.
+         * This prevents people from spamming the it.
+         */
+        if ( $avatar_id ==! "" ) {
+            wp_delete_attachment( $avatar_id, false );
+        }
+
+        $attachment_id = media_handle_sideload( $_FILES['files'], 0 );
+        update_user_meta( $user_id, '_peerraiser_custom_avatar', $attachment_id );
+
+        $event->set_result(
+            array(
+                'avatar_id' => $attachment_id,
+                'success'   => true,
+                'image_url' => wp_get_attachment_image_url( $attachment_id, 'peerraiser_campaign_thumbnail' )
+            )
+        );
+    }
+
 }
