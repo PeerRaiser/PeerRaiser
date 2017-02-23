0fb68a2f804ce36b00706092000fc0c95748b0ac
Nathan Allen <email@ncallen.com>
2017-02-22 23:58:12 -0500

    Add view for 'Add New Campaign' screen

---
+++ views/backend/campaign-add.php
@@ -0,0 +1,38 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    // prevent direct access to this file
+    exit;
+}
+?>
+<div class="wrap">
+    <div id="peerraiser-js-message" class="pr_flash-message" style="display:none;">
+        <p></p>
+    </div>
+
+    <h1 class="wp-heading-inline"><?php _e( 'Add New Campaign', 'peerraiser' ); ?></h1>
+    <hr class="wp-header-end">
+
+    <form id="peerraiser-add-campaign" action="post">
+
+        <div id="poststuff">
+            <div id="post-body" class="metabox-holder columns-2">
+                <div id="postbox-container-1" class="postbox-container">
+                    sidebar
+                </div>
+                <div id="postbox-container-2" class="postbox-container">
+                    <?php
+                    //$cmb = cmb2_get_metabox( 'peerraiser-campaign', 'peerraiser-campaign' );
+
+                    //echo "<pre>" . print_r( $cmb,1 ) . "</pre>";
+
+                    echo cmb2_get_metabox_form( 'peerraiser-campaign', 0, array( 'form_format' => '', ) );
+
+                    ?>
+                </div>
+            </div>
+        </div>
+
+
+    </form>
+
+</div>
