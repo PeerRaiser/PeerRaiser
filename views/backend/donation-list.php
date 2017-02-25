5f505013b13dbb2eac2cdc1e8f660631a116f81f
Nathan Allen <email@ncallen.com>
2017-02-24 23:48:51 -0500

    Add button to 'Add Offline Donation'

--- views/backend/donation-list.php
+++ views/backend/donation-list.php
@@ -9,10 +9,15 @@ if ( ! defined( 'ABSPATH' ) ) {
         <p></p>
     </div>
 
-    <h1 class="wp-heading-inline"><?php _e( 'Donations', 'heart' ); ?></h1>
+    <h1 class="wp-heading-inline"><?php _e( 'Donations', 'peerraiser' ); ?></h1>
+    <a href="<?php echo admin_url( 'admin.php?page=peerraiser-donations&view=add' ); ?>" class="page-title-action"><?php _e( 'Add Offline Donation', 'peerraiser' ); ?></a>
+    <hr class="wp-header-end">
 
-    <?php
-    $peerraiser['list_table']->prepare_items();
-    $peerraiser['list_table']->display();
-    ?>
+    <?php $peerraiser['list_table']->views(); ?>
+    <form method="post">
+        <?php
+        $peerraiser['list_table']->prepare_items();
+        $peerraiser['list_table']->display();
+        ?>
+    </form>
 </div>
