7e925153578700902df730d96aa19c1bfa446987
Nathan Allen <email@ncallen.com>
2017-03-05 23:19:40 -0500

    Add donation notes box

---
+++ views/backend/partials/donation-box-notes.php
@@ -0,0 +1,9 @@
+<div id="donation-options" class="postbox">
+    <h2><span><?php _e( 'Donation Notes', 'peerraiser' ); ?></span></h2>
+    <div id="donation-notes">
+
+    </div>
+    <div class="inside">
+        <textarea name="_pr_donation_note" id="donation-note" class="large-text" rows="5"></textarea>
+    </div>
+</div>
