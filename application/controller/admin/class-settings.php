fede7ce51cf1aec87c7115aacfdc9a39e5e11e5d
Nathan Allen <email@ncallen.com>
2017-03-12 15:44:11 -0400

    Fix settings now handling ajax correctly on save

--- application/controller/admin/class-settings.php
+++ application/controller/admin/class-settings.php
@@ -3,11 +3,14 @@
 namespace PeerRaiser\Controller\Admin;
 
 use \PeerRaiser\Controller\Base;
+use \PeerRaiser\Core\Setup;
+use \PeerRaiser\Helper\Text;
 
 class Settings extends Base {
 
     public function register_actions() {
         add_action( 'wp_ajax_peerraiser_update_settings', array( $this, 'ajax_update_settings' ) );
+        add_action( 'wp_ajax_peerraiser_update_settings', array( $this, 'peerraiser_on_ajax_send_json' ) );
         add_action( 'wordpress_init',                     array( $this, 'add_thumbnail_sizes' ) );
         add_action( 'wordpress_init',                     array( $this, 'maybe_flush_rewrite_rules' ) );
     }
@@ -46,39 +49,38 @@ class Settings extends Base {
 
         wp_register_style(
             'peerraiser-ladda',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'vendor/ladda.min.css',
+            Setup::get_plugin_config()->get('css_url') . 'vendor/ladda.min.css',
             array(),
             '4.0.2'
         );
         wp_register_style(
             'peerraiser-admin-settings',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin-settings.css',
+            Setup::get_plugin_config()->get('css_url') . 'peerraiser-admin-settings.css',
             array('peerraiser-ladda'),
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('version')
+            Setup::get_plugin_config()->get('version')
         );
         wp_enqueue_style( 'peerraiser-admin-fundraisers' );
         wp_enqueue_style( 'peerraiser-ladda' );
         wp_enqueue_style( 'peerraiser-admin-settings' );
         wp_enqueue_style( 'peerraiser-font-awesome' );
 
-
         wp_register_script(
             'peerraiser-admin-settings',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('js_url') . 'peerraiser-admin-settings.js',
+            Setup::get_plugin_config()->get('js_url') . 'peerraiser-admin-settings.js',
             array( 'jquery', 'peerraiser-ladda', 'peerraiser-spin' ),
             '1.0.0',
             true
         );
         wp_register_script(
             'peerraiser-ladda',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('js_url') . 'vendor/ladda.min.js',
+            Setup::get_plugin_config()->get('js_url') . 'vendor/ladda.min.js',
             array( 'peerraiser-spin' ),
             '1.0.0',
             true
         );
         wp_register_script(
             'peerraiser-spin',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get('js_url') . 'vendor/spin.min.js',
+            Setup::get_plugin_config()->get('js_url') . 'vendor/spin.min.js',
             array(),
             '1.0.0',
             true
@@ -99,7 +101,6 @@ class Settings extends Base {
 
     }
 
-
     public function register_meta_boxes() {
 
         $settings_model        = new \PeerRaiser\Model\Admin\Settings();
@@ -118,26 +119,23 @@ class Settings extends Base {
 
     }
 
-
     public function ajax_update_settings() {
         check_ajax_referer($_POST['none_name']);
 
         $model = new \PeerRaiser\Model\Admin\Settings();
         $default_fields = $model->get_field_names();
 
-        $event->set_result(
-            array(
-                'success' => false,
-                'message' => __( 'An error occurred when trying to retrieve the information. Please try again.', 'peerraiser' ),
-            )
-        );
+        $result =  array(
+			'success' => false,
+			'message' => __( 'An error occurred when trying to retrieve the information. Please try again.', 'peerraiser' ),
+		);
 
-        $formData = $_POST['formData'];
-        $plugin_options = get_option( 'peerraiser_options', array() );
+        $formData         = $_POST['formData'];
+        $plugin_options   = get_option( 'peerraiser_options', array() );
         $settings_updated = 0;
 
-        foreach ($formData as $data) {
-            if ( in_array($data['name'], $default_fields) ) {
+        foreach ( $formData as $data ) {
+            if ( in_array( $data['name'], $default_fields ) ) {
                 $plugin_options[$data['name']] = $data['value'];
                 $settings_updated++;
             }
@@ -154,9 +152,10 @@ class Settings extends Base {
             // settings => json_encode( $plugin_options ),
         );
 
-        $event->set_result( $data );
-    }
+		echo Text::peerraiser_json_encode( $data );
 
+		wp_die();
+    }
 
     /**
      * If the plugin settings have recently been updated, flush the rewrite rules.
@@ -172,14 +171,12 @@ class Settings extends Base {
         }
     }
 
-
     public function get_tabs() {
         $settings_model = new \PeerRaiser\Model\Admin\Settings();
         $default_tabs = $settings_model->get_settings_tabs();
 
         return apply_filters( 'peerraiser_settings_tabs', $default_tabs );
     }
-
 
     public function get_sections() {
         $settings_model = new \PeerRaiser\Model\Admin\Settings();
@@ -188,14 +185,12 @@ class Settings extends Base {
         return apply_filters( 'peerraiser_settings_sections', $default_sections );
     }
 
-
     public function get_settings_content( $active_tab, $active_section ) {
         $settings_model = new \PeerRaiser\Model\Admin\Settings();
         $default_content = $settings_model->get_settings_content( $active_tab, $active_section );
 
         return apply_filters( 'peerraiser_settings_content', $default_content );
     }
-
 
     public function add_thumbnail_sizes() {
         add_image_size( 'peerraiser_campaign_thumbnail', 150, 150, true );
