3f5bac27e365b2f7a982537a6a2b85d87f739c8f
Nate Allen <email@ncallen.com>
2017-03-29 19:03:14 -0400

    Output donation notes

--- views/backend/partials/donation-box-notes.php
+++ views/backend/partials/donation-box-notes.php
@@ -1,9 +1,16 @@
 <div id="donation-options" class="postbox">
     <h2><span><?php _e( 'Donation Notes', 'peerraiser' ); ?></span></h2>
-    <div id="donation-notes">
-
-    </div>
     <div class="inside">
         <textarea name="_peerraiser_donation_note" id="donation-note" class="large-text" rows="5"></textarea>
     </div>
+
+    <?php if ( ! empty( $peerraiser['donation']->notes ) ) : ?>
+        <?php print_r( $peerraiser['donation']->notes ) ?>
+        <div id="donation-notes">
+            <?php foreach ( $peerraiser['donation']->notes as $donation_note ) : ?>
+                <p>On <strong><?php echo $donation_note['time']; ?></strong>:</p>
+                <p><?php echo wpautop( $donation_note['note'] ); ?></p>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 </div>
