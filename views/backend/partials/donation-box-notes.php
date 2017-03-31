66b00dde0871d7a6a68c926c271015b64c278d7a
Nate Allen <email@ncallen.com>
2017-03-30 22:03:27 -0400

    Donation note date format based on WordPress options

--- views/backend/partials/donation-box-notes.php
+++ views/backend/partials/donation-box-notes.php
@@ -5,9 +5,11 @@
 
         <?php if ( ! empty( $peerraiser['donation']->notes ) ) : ?>
             <div id="donation-notes">
+                <?php $date_format = get_option( 'date_format' ) . ', ' . get_option( 'time_format' ) ?>
                 <?php foreach ( $peerraiser['donation']->notes as $donation_note ) : ?>
-                    <p>On <strong><?php echo $donation_note['time']; ?></strong>:</p>
-                    <p><?php echo wpautop( $donation_note['note'] ); ?></p>
+                    <p><strong><?php printf( esc_html__( '%1$s on %2$s', 'peerraiser' ), $donation_note['who'], mysql2date( $date_format, $donation_note['when'] ) ); ?></strong>:</p>
+
+                    <p><?php echo wpautop( $donation_note['what'] ); ?></p>
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
