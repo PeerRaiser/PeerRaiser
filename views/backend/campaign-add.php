a3dae1e64a8c58f744c7cec90800dd646c4238ff
Nate Allen <email@ncallen.com>
2017-04-15 19:43:07 -0400

    Add campaign status field

--- views/backend/campaign-add.php
+++ views/backend/campaign-add.php
@@ -21,9 +21,18 @@ if ( ! defined( 'ABSPATH' ) ) {
                             <h2><span>Publish</span></h2>
                             <div class="inside">
                                 <div class="submitbox" id="submitpost">
-                                    <div id="misc-publishing-actions">
-                                        <div class="misc-pub-section">
-                                            <input type="checkbox" id="is-public" value="is_public" checked> <label for="is-public">Display publicly</label>
+                                    <div class="misc-pub-section campaign-status active">
+		                                <?php _e( 'Status:', 'peerraiser' ); ?> <strong><?php _e( 'Active', 'peerraiser' ); ?></strong>
+                                        <a href="#campaign_status" class="edit-campaign-status hide-if-no-js" role="button"><span aria-hidden="true"><?php _e( 'Edit', 'peerraiser') ?></span> <span class="screen-reader-text"><?php _e( 'Edit status', 'peerraiser' ); ?></span></a>
+                                        <div id="campaign-status-select" class="hide-if-js">
+                                            <input type="hidden" name="_peerraiser_campaign_status_hidden" value="active">
+                                            <select name="_peerraiser_campaign_status" id="campaign-status">
+		                                        <?php foreach ( $peerraiser['campaign_admin']->get_campaign_statuses() as $key => $value ) : ?>
+                                                    <option value="<?php echo $key ?>" <?php selected( $campaign_status, $key, true ); ?>><?php echo $value ?></option>
+		                                        <?php endforeach; ?>
+                                            </select>
+                                            <a href="#campaign_status" class="save hide-if-no-js button"><?php _e( 'OK', 'peerraiser' ); ?></a>
+                                            <a href="#campaign_status" class="cancel hide-if-no-js button-cancel"><?php _e( 'Cancel', 'peerraiser' ); ?></a>
                                         </div>
                                     </div>
                                     <div id="major-publishing-actions">
