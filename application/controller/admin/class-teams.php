55c4e8c20cb36fab8331490912a7bc774fa9644c
Nate Allen <email@ncallen.com>
2017-04-18 22:51:00 -0400

    Add handle_update_team method

--- application/controller/admin/class-teams.php
+++ application/controller/admin/class-teams.php
@@ -8,10 +8,11 @@ use \PeerRaiser\Model\Admin\Admin_Notices as Admin_Notices_Model;
 class Teams extends \PeerRaiser\Controller\Base {
 
     public function register_actions() {
-        add_action( 'cmb2_admin_init',                    array( $this, 'register_meta_boxes' ) );
-		add_action( 'peerraiser_page_peerraiser-teams',   array( $this, 'load_assets' ) );
-        add_action( 'peerraiser_add_team',	              array( $this, 'handle_add_team' ) );
-	    add_action( 'peerraiser_delete_team',             array( $this, 'delete_team' ) );
+        add_action( 'cmb2_admin_init',                  array( $this, 'register_meta_boxes' ) );
+		add_action( 'peerraiser_page_peerraiser-teams', array( $this, 'load_assets' ) );
+        add_action( 'peerraiser_add_team',	            array( $this, 'handle_add_team' ) );
+        add_action( 'peerraiser_update_team',           array( $this, 'handle_update_team' ) );
+	    add_action( 'peerraiser_delete_team',           array( $this, 'delete_team' ) );
     }
 
     /**
@@ -32,11 +33,11 @@ class Teams extends \PeerRaiser\Controller\Base {
         $view = in_array( $view, $default_views ) ? $view : apply_filters( 'peerraiser_donation_admin_view', 'list', $view );
 
         $view_args = array(
-            'currency_symbol'      => $currency_symbol,
-            'standard_currency'    => $plugin_options['currency'],
-            'admin_url'            => get_admin_url(),
-            'list_table'           => new \PeerRaiser\Model\Admin\Team_List_Table(),
-            'team_admin'       => new \PeerRaiser\Model\Admin\Teams()
+	        'currency_symbol'   => $currency_symbol,
+	        'standard_currency' => $plugin_options['currency'],
+	        'admin_url'         => get_admin_url(),
+	        'list_table'        => new \PeerRaiser\Model\Admin\Team_List_Table(),
+	        'team_admin'        => new \PeerRaiser\Model\Admin\Teams()
         );
 
 	    if ( $view === 'summary' ) {
@@ -53,24 +54,24 @@ class Teams extends \PeerRaiser\Controller\Base {
         $teams_model = new \PeerRaiser\Model\Admin\Teams();
         $team_field_groups = $teams_model->get_fields();
 
-        foreach ($team_field_groups as $field_group) {
-            $cmb = new_cmb2_box( array(
-                'id'           => $field_group['id'],
-                'title'         => $field_group['title'],
-                'object_types'  => array( 'pr_team' ),
-                'context'       => $field_group['context'],
-                'priority'      => $field_group['priority'],
-            ) );
-            foreach ($field_group['fields'] as $key => $value) {
-                if ( $key === 'team_campaign' && $this->is_edit_page( 'edit' ) ){
-                    $value['type'] = 'text';
-                    $value['attributes'] = array(
-                        'readonly' => 'readonly',
-                    );
-                }
-                $cmb->add_field($value);
-            }
-        }
+	    foreach ( $team_field_groups as $field_group ) {
+		    $cmb = new_cmb2_box( array(
+			    'id'           => $field_group['id'],
+			    'title'        => $field_group['title'],
+			    'object_types' => array( 'pr_team' ),
+			    'context'      => $field_group['context'],
+			    'priority'     => $field_group['priority'],
+		    ) );
+		    foreach ( $field_group['fields'] as $key => $value ) {
+			    if ( $key === 'team_campaign' && $this->is_edit_page( 'edit' ) ) {
+				    $value['type']       = 'text';
+				    $value['attributes'] = array(
+					    'readonly' => 'readonly',
+				    );
+			    }
+			    $cmb->add_field( $value );
+		    }
+	    }
 
     }
 
@@ -119,63 +120,89 @@ class Teams extends \PeerRaiser\Controller\Base {
 
     }
 
-    public function display_fundraisers_list() {
-        global $post;
-        $paged = isset($_GET['fundraisers_page']) ? $_GET['fundraisers_page'] : 1;
+	public function display_fundraisers_list() {
+		global $post;
+		$paged = isset( $_GET['fundraisers_page'] ) ? $_GET['fundraisers_page'] : 1;
 
-        $teams_model = new \PeerRaiser\Model\Admin\Teams();
-        $team_fundraisers = $teams_model->get_fundraisers( $post->ID, $paged );
+		$teams_model      = new \PeerRaiser\Model\Admin\Teams();
+		$team_fundraisers = $teams_model->get_fundraisers( $post->ID, $paged );
 
-        $plugin_options = get_option( 'peerraiser_options', array() );
-        $currency = new \PeerRaiser\Model\Currency();
-        $currency_symbol = $currency->get_currency_symbol_by_iso4217_code($plugin_options['currency']);
+		$plugin_options  = get_option( 'peerraiser_options', array() );
+		$currency        = new \PeerRaiser\Model\Currency();
+		$currency_symbol = $currency->get_currency_symbol_by_iso4217_code( $plugin_options['currency'] );
 
-        $args = array(
-            'custom_query' => $team_fundraisers,
-            'paged' => isset($_GET['fundraisers_page']) ? $_GET['fundraisers_page'] : 1,
-            'paged_name' => 'fundraisers_page'
-        );
-        $pagination = \PeerRaiser\Helper\View::get_admin_pagination( $args );
+		$args       = array(
+			'custom_query' => $team_fundraisers,
+			'paged'        => isset( $_GET['fundraisers_page'] ) ? $_GET['fundraisers_page'] : 1,
+			'paged_name'   => 'fundraisers_page'
+		);
+		$pagination = \PeerRaiser\Helper\View::get_admin_pagination( $args );
 
-        $view_args = array(
-            'number_of_fundraisers' => $team_fundraisers->found_posts,
-            'pagination'            => $pagination,
-            'currency_symbol'       => $currency_symbol,
-            'fundraisers'           => $team_fundraisers->get_posts(),
-        );
+		$view_args = array(
+			'number_of_fundraisers' => $team_fundraisers->found_posts,
+			'pagination'            => $pagination,
+			'currency_symbol'       => $currency_symbol,
+			'fundraisers'           => $team_fundraisers->get_posts(),
+		);
 
-        $this->assign( 'peerraiser', $view_args );
+		$this->assign( 'peerraiser', $view_args );
 
-        $this->render( 'backend/partials/team-fundraisers' );
-    }
+		$this->render( 'backend/partials/team-fundraisers' );
+	}
 
-    public function handle_add_team() {
-        if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'peerraiser_add_team_nonce' ) ) {
-            die( __('Security check failed.', 'peerraiser' ) );
-        }
+	public function handle_add_team() {
+		if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'peerraiser_add_team_nonce' ) ) {
+			die( __( 'Security check failed.', 'peerraiser' ) );
+		}
 
-        $validation = $this->is_valid_team();
-        if ( ! $validation['is_valid'] ) {
-            return;
-        }
+		$validation = $this->is_valid_team();
+		if ( ! $validation['is_valid'] ) {
+			return;
+		}
 
-        $team = new Team_Model();
+		$team = new Team_Model();
 
-	    $this->add_fields( $team );
+		$this->add_fields( $team );
 
-        // Save to the database
-        $team->save();
+		// Save to the database
+		$team->save();
 
-        // Create redirect URL
-        $location = add_query_arg( array(
-            'page' => 'peerraiser-teams',
-            'view' => 'summary',
-            'team' => $team->ID
-        ), admin_url( 'admin.php' ) );
+		// Create redirect URL
+		$location = add_query_arg( array(
+			'page' => 'peerraiser-teams',
+			'view' => 'summary',
+			'team' => $team->ID
+		), admin_url( 'admin.php' ) );
 
-        // Redirect to the edit screen for this new donation
-        wp_safe_redirect( $location );
-    }
+		// Redirect to the edit screen for this new donation
+		wp_safe_redirect( $location );
+	}
+
+	public function handle_update_team() {
+		if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'peerraiser_update_team_' . $_REQUEST['team_id'] ) ) {
+			die( __( 'Security check failed.', 'peerraiser' ) );
+		}
+
+		$validation = $this->is_valid_team();
+		if ( ! $validation['is_valid'] ) {
+			return;
+		}
+
+		$team = new Team_Model( $_REQUEST['team_id'] );
+
+		$this->update_fields( $team );
+		$team->save();
+
+		// Create redirect URL
+		$location = add_query_arg( array(
+			'page' => 'peerraiser-teams',
+			'view' => 'summary',
+			'team' => $team->ID
+		), admin_url( 'admin.php' ) );
+
+		// Redirect to the edit screen for this new donation
+		wp_safe_redirect( $location );
+	}
 
     /**
      * Checks if the fields are valid
@@ -184,44 +211,46 @@ class Teams extends \PeerRaiser\Controller\Base {
      * @since     1.0.0
      * @return    array    Array with 'is_valid' of TRUE or FALSE and 'field_errors' with any error messages
      */
-    private function is_valid_team() {
-	    $teams_model = new \PeerRaiser\Model\Admin\Teams();
-	    $required_fields = $teams_model->get_required_field_ids();
+	private function is_valid_team() {
+		$teams_model     = new \PeerRaiser\Model\Admin\Teams();
+		$required_fields = $teams_model->get_required_field_ids();
 
-        $data = array(
-            'is_valid'     => true,
-            'field_errors' => array(),
-        );
+		$data = array(
+			'is_valid'     => true,
+			'field_errors' => array(),
+		);
 
-        // Make sure team name isn't already taken
-        $team_exists = term_exists( $_REQUEST['_peerraiser_team_name'], 'peerraiser_team' );
+		// Make sure team name isn't already taken
+		if ( isset( $_REQUEST['peerraiser_action'] ) && 'add_team' === $_REQUEST['peerraiser_action'] ) {
+			$team_exists = term_exists( $_REQUEST['_peerraiser_team_name'], 'peerraiser_team' );
 
-        if ( $team_exists !== 0 && $team_exists !== null ) {
-            $data['field_errors'][ '_peerraiser_team_name' ] = __( 'This team name already exists', 'peerraiser' );
-        }
+			if ( $team_exists !== 0 && $team_exists !== null ) {
+				$data['field_errors']['_peerraiser_team_name'] = __( 'This team name already exists', 'peerraiser' );
+			}
+		}
 
-        // Check required fields
-        foreach ( $required_fields as $field ) {
-            if ( ! isset( $_REQUEST[ $field ] ) || empty( $_REQUEST[ $field ] ) ) {
-                $data['field_errors'][ $field ] = __( 'This field is required.', 'peerraiser' );
-            }
-        }
+		// Check required fields
+		foreach ( $required_fields as $field ) {
+			if ( ! isset( $_REQUEST[ $field ] ) || empty( $_REQUEST[ $field ] ) ) {
+				$data['field_errors'][ $field ] = __( 'This field is required.', 'peerraiser' );
+			}
+		}
 
-        if ( ! empty( $data['field_errors'] ) ) {
-            $message = __( 'There was an issue creating this team. Please fix the errors below.', 'peerraiser' );
-            Admin_Notices_Model::add_notice( $message, 'notice-error', true );
+		if ( ! empty( $data['field_errors'] ) ) {
+			$message = __( 'There was an issue creating this team. Please fix the errors below.', 'peerraiser' );
+			Admin_Notices_Model::add_notice( $message, 'notice-error', true );
 
-            wp_localize_script(
-                'jquery',
-                'peerraiser_field_errors',
-                $data['field_errors']
-            );
+			wp_localize_script(
+				'jquery',
+				'peerraiser_field_errors',
+				$data['field_errors']
+			);
 
-            $data['is_valid'] = false;
-        }
+			$data['is_valid'] = false;
+		}
 
-        return $data;
-    }
+		return $data;
+	}
 
 	/**
 	 * Handle "delete campaign" action
@@ -230,7 +259,7 @@ class Teams extends \PeerRaiser\Controller\Base {
 	 */
 	public function delete_team() {
 		if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'peerraiser_delete_team_' . $_REQUEST['team_id'] ) ) {
-			die( __('Security check failed.', 'peerraiser' ) );
+			die( __( 'Security check failed.', 'peerraiser' ) );
 		}
 
 		// Delete the campaign
@@ -246,13 +275,13 @@ class Teams extends \PeerRaiser\Controller\Base {
 		wp_safe_redirect( $location );
 	}
 
-	private function add_fields( $team) {
+	private function add_fields( $team ) {
 		$teams_model = new \PeerRaiser\Model\Admin\Teams();
 
-		$field_ids   = $teams_model->get_field_ids();
+		$field_ids = $teams_model->get_field_ids();
 
 		// Add team name to field list, since its not a CMB2 field
-		$field_ids['team_name']   = '_peerraiser_team_name';
+		$field_ids['team_name'] = '_peerraiser_team_name';
 
 		foreach ( $field_ids as $key => $value ) {
 			switch ( $value ) {
@@ -260,8 +289,8 @@ class Teams extends \PeerRaiser\Controller\Base {
 					$team->team_name = $_REQUEST['_peerraiser_team_name'];
 					break;
 				default :
-					if ( isset( $_REQUEST[$value] ) ) {
-						$team->$key = $_REQUEST[$value];
+					if ( isset( $_REQUEST[ $value ] ) ) {
+						$team->$key = $_REQUEST[ $value ];
 					}
 					break;
 			}
@@ -271,7 +300,7 @@ class Teams extends \PeerRaiser\Controller\Base {
 	private function update_fields( $team ) {
 		$teams_model = new \PeerRaiser\Model\Admin\Teams();
 
-		$field_ids   = $teams_model->get_field_ids();
+		$field_ids = $teams_model->get_field_ids();
 
 		if ( isset( $_REQUEST['_peerraiser_team_name'] ) ) {
 			$team->update_team_name( $_REQUEST['_peerraiser_team_name'] );
@@ -280,10 +309,10 @@ class Teams extends \PeerRaiser\Controller\Base {
 		$current = $team->get_meta();
 
 		foreach ( $field_ids as $key => $value ) {
-			if ( isset( $_REQUEST[$value] ) && $_REQUEST[$value] !== $current[$value][0] ) {
-				$team->$key = $_REQUEST[$value];
-			} elseif ( ! isset( $_REQUEST[$value] ) ) {
-				$team->delete_meta($value);
+			if ( isset( $_REQUEST[ $value ] ) && $_REQUEST[ $value ] !== $current[ $value ][0] ) {
+				$team->$key = $_REQUEST[ $value ];
+			} elseif ( ! isset( $_REQUEST[ $value ] ) ) {
+				$team->delete_meta( $value );
 			}
 		}
 	}
