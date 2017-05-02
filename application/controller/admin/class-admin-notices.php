3752247019b90d34008d76ed7b7f17891c752e39
Nate Allen <email@ncallen.com>
2017-05-01 22:40:17 -0400

    Add method to display notice when test mode is active

--- application/controller/admin/class-admin-notices.php
+++ application/controller/admin/class-admin-notices.php
@@ -4,7 +4,8 @@ namespace PeerRaiser\Controller\Admin;
 class Admin_Notices extends \PeerRaiser\Controller\Base {
 
     public function register_actions() {
-        add_action( 'admin_notices', array( $this, 'display_notices' ) );
+        add_action( 'admin_notices',  array( $this, 'display_notices' ) );
+	    add_action( 'admin_bar_menu', array( $this, 'maybe_display_test_mode_notice' ), 1000, 1 );
     }
 
     public function display_notices() {
@@ -18,4 +19,27 @@ class Admin_Notices extends \PeerRaiser\Controller\Base {
             <?php
         }
     }
+
+	/**
+	 * Display admin bar when active.
+	 *
+	 * @param WP_Admin_Bar $wp_admin_bar WP_Admin_Bar instance, passed by reference.
+	 *
+	 * @return bool
+	 */
+	public function maybe_display_test_mode_notice( $wp_admin_bar ) {
+		if ( peerraiser_get_option( 'test_mode' ) !== 'true' ) {
+			return false;
+		}
+
+		// Add the main siteadmin menu item.
+		$wp_admin_bar->add_menu( array(
+			'id'     => 'peerraiser-test-notice',
+			'href'   => admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways' ),
+			'parent' => 'top-secondary',
+			'title'  => esc_html__( 'PeerRaiser Test Mode Active', 'peerraiser' ),
+			'meta'   => array( 'class' => 'peerraiser-test-mode-active' ),
+		) );
+
+	}
 }
