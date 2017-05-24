8ceaa43396f3a863e27f46e34f2d41cce80d9ba0
Nate Allen <email@ncallen.com>
2017-05-24 11:57:03 -0400

    Add template for widget form

---
+++ views/backend/widget/peerraiser-campaign-donate-button.php
@@ -0,0 +1,4 @@
+<p>
+	<label for="<?php echo esc_attr( $this->get_field_id( 'button_label' ) ); ?>"><?php esc_attr_e( 'Button Title:', 'peerraiser' ); ?></label>
+	<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'button_label' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'button_label' ) ); ?>" type="text" value="<?php echo esc_attr( $peerraiser['button_label'] ); ?>" placeholder="<?php _e( 'i.e. Donate to this campaign', 'peerraiser'); ?>">
+</p>
