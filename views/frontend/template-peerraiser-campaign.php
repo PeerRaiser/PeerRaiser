f0640d0168ba00f622028887ba1b994a2680fed1
Nate Allen <email@ncallen.com>
2017-05-24 11:55:37 -0400

    Add structure for campaign template

--- views/frontend/template-peerraiser-campaign.php
+++ views/frontend/template-peerraiser-campaign.php
@@ -2,15 +2,39 @@
 /**
  * The template for displaying the PeerRaiser Campaign
  */
+$campaign = peerraiser_get_current_campaign();
 ?>
 
 <?php get_header(); ?>
 
-<?php $campaign = peerraiser_get_current_campaign(); ?>
+<div id="peerraiser-campaign" class="<?php echo 'peerraiser-campaign-' . $campaign->ID ?> <?php echo 'peerraiser-campaign-' . $campaign->campaign_slug; ?>">
 
-<h1><?php echo $campaign->campaign_name; ?></h1>
+    <div class="peerraiser-campaign-content<?php if ( is_active_sidebar( 'peerraiser-campaign-sidebar' ) ) echo ' has-sidebar'; ?>">
 
-<p><?php echo $campaign->campaign_description; ?></p>
+        <?php if ( $campaign->banner_image ) : ?>
+            <div class="peerraiser-campaign-banner">
+                <img src="<?php echo $campaign->banner_image; ?>" alt="<?php echo $campaign->campaign_name; ?>">
+            </div>
+        <?php endif; ?>
 
-<?php get_sidebar(); ?>
+        <h1 class="peerraiser-campaign-title"><?php echo $campaign->campaign_name; ?></h1>
+
+        <div class="peerraiser-campaign-description">
+            <?php echo $campaign->campaign_description; ?>
+        </div>
+
+    </div>
+
+    <?php if ( is_active_sidebar( 'peerraiser-campaign-sidebar' ) ) : ?>
+
+        <div class="peerraiser-campaign-sidebar">
+
+	        <?php dynamic_sidebar( 'peerraiser-campaign-sidebar' ); ?>
+
+        </div>
+
+    <?php endif; ?>
+
+</div>
+
 <?php get_footer(); ?>
