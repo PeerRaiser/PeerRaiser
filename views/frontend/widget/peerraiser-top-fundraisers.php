88178cac4f69f373f931f46e6330e24e495046e8
Nate Allen <email@ncallen.com>
2017-05-26 09:58:09 -0400

    Add Top Teams widget model

--- views/frontend/widget/peerraiser-top-fundraisers.php
+++ views/frontend/widget/peerraiser-top-fundraisers.php
@@ -10,7 +10,6 @@
     <ol>
         <?php foreach ( $top_fundraisers as $fundraiser ) : ?>
             <li><a href="<?php echo get_the_permalink( $fundraiser->ID ); ?>"><?php echo $fundraiser->fundraiser_name; ?></a> (<?php echo peerraiser_money_format( $fundraiser->donation_value ); ?>)</li>
-            <?php //print_r($fundraiser); ?>
         <?php endforeach; ?>
     </ol>
 <?php endif; ?>
