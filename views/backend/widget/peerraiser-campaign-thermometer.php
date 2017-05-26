1bf2ba43dd07e2aa0d3aeb97d0d72542315260f1
Nate Allen <email@ncallen.com>
2017-05-26 11:23:24 -0400

    Add campaign thermometer backend template

---
+++ views/backend/widget/peerraiser-campaign-thermometer.php
@@ -0,0 +1,4 @@
+<p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'peerraiser' ); ?></label>
+    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $peerraiser['title'] ); ?>">
+</p>
