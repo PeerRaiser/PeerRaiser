a46a66cff073cb8a24683540a381bdeb6b89861d
Nate Allen <email@ncallen.com>
2017-05-27 16:58:37 -0400

    Add form fields to donation form

--- views/frontend/donation-form.php
+++ views/frontend/donation-form.php
@@ -1,18 +1,74 @@
-<form>
+<form class="peerraiser-donation-form">
 	<section class="peerraiser-donation-amounts">
 		<label><h3><?php _e( 'Choose a Donation Amount', 'peerraiser' ); ?> </h3></label>
+		<?php // TODO: Add setting for donation amounts instead of hard coding them ?>
 		<ul class="peerraiser-donation-amount-buttons">
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="25" class="peerraiser-donation-button" id="button_1"><label for="button_1">$25</label>
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="50" class="peerraiser-donation-button" id="button_2"><label for="button_2">$50</label>
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="100" class="peerraiser-donation-button" id="button_3"><label for="button_3">$100</label>
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="250" class="peerraiser-donation-button" id="button_4"><label for="button_4">$250</label>
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="500" class="peerraiser-donation-button" id="button_5"><label for="button_5">$500</label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="25" class="peerraiser-donation-button" id="button_1"><label for="button_1"><?php echo peerraiser_money_format( 25, true, false ); ?></label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="50" class="peerraiser-donation-button" id="button_2"><label for="button_2"><?php echo peerraiser_money_format( 50, true, false ); ?></label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="100" class="peerraiser-donation-button" id="button_3"><label for="button_3"><?php echo peerraiser_money_format( 100, true, false ); ?></label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="250" class="peerraiser-donation-button" id="button_4"><label for="button_4"><?php echo peerraiser_money_format( 250, true, false ); ?></label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="500" class="peerraiser-donation-button" id="button_5"><label for="button_5"><?php echo peerraiser_money_format( 500, true, false ); ?></label>
 		</ul>
-		<div class="row collapse">
-			<div class="peerraiser-donation-amount-other">
-				<span class="peerraiser-currency-symbol">$</span>
-				<input class="peerraiser-donation-input" value="" maxlength="8" type="number" autocomplete="off" placeholder="Other Amount ($10 Min)" data-minimum="10" aria-label="Enter Donation Amount USD Min. $10">
-			</div>
+		<div class="peerraiser-donation-amount-other">
+			<?php if ( $peerraiser['currency_position'] === 'before' ) : ?>
+				<span class="peerraiser-currency-symbol"><?php echo $peerraiser['currency_symbol']; ?></span>
+			<?php endif; ?>
+
+
+			<?php if ( $peerraiser['currency_position'] === 'before' ) : ?>
+				<?php /* translators: 1: Currency symbol 2: Donation amount */ ?>
+				<?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), $peerraiser['currency_symbol'], '10' ); ?>
+			<?php else : ?>
+				<?php /* translators: 1: Donation amount 2: Currency symbol  */ ?>
+				<?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), '10', $peerraiser['currency_symbol'] ); ?>
+			<?php endif; ?>
+			<input class="peerraiser-donation-input" value="" maxlength="8" type="number" autocomplete="off" placeholder="<?php echo $field_placeholder; ?>" data-minimum="10" aria-label="Enter Donation Amount USD Min. $10">
+
+			<?php if ( $peerraiser['currency_position'] === 'after' ) : ?>
+				<span class="peerraiser-currency-symbol"><?php echo $peerraiser['currency_symbol']; ?></span>
+			<?php endif; ?>
 		</div>
 	</section>
+
+	<section class="donor-about">
+		<h3><?php apply_filters( 'peerraiser_donation_form_about_title', _e( 'Tell us a little about yourself', 'peerraiser' ) ); ?> </h3>
+
+		<div id="peerraiser_field_first_name" class="peerraiser-field peerraiser-field-text required">
+			<label for="peerraiser_first_name">
+				<?php _e( 'First Name', 'peerraiser' ); ?> <span class="required">*</span>
+			</label>
+			<input type="text" name="first_name" id="peerraiser_first_name" value="">
+		</div>
+
+		<div id="peerraiser_field_last_name" class="peerraiser-field peerraiser-field-text">
+			<label for="peerraiser_last_name">
+				<?php _e( 'Last Name', 'peerraiser' ); ?>
+			</label>
+			<input type="text" name="last_name" id="peerraiser_last_name" value="">
+		</div>
+
+		<div id="peerraiser_field_email_address" class="peerraiser-field peerraiser-field-text required">
+			<label for="peerraiser_email_address">
+				<?php _e( 'Email Address', 'peerraiser' ); ?> <span class="required">*</span>
+			</label>
+			<input type="email" name="email_address" id="peerraiser_email_address" value="">
+		</div>
+
+		<div id="peerraiser_field_public_name" class="peerraiser-field peerraiser-field-text">
+			<label for="peerraiser_public_name">
+				<?php _e( 'Display my name publicly as...', 'peerraiser' ); ?>
+			</label>
+			<input type="text" name="public_name" id="peerraiser_public_name" value="">
+		</div>
+
+		<div id="peerraiser_field_anonymous" class="peerraiser-field peerraiser-field-checkbox">
+			<label for="peerraiser_anonymous">
+				<input type="checkbox" name="anonymous" id="peerraiser_anonymous" value="true"><?php _e( "Don't display my name", 'peerraiser' ); ?>
+			</label>
+		</div>
+	</section>
+
+	<button type="submit" class="peerraiser-donate-submit peerraiser-submit-button"><?php _e( 'Donate','peerraiser' ); ?></button>
+
+	<input type="hidden" name="peerraiser_action" value="add_pending_donation">
 </form>
