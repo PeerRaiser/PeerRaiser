d41b3d82f6d7fff40d86bff59af17a3ca608419e
Nate Allen <email@ncallen.com>
2017-07-01 12:33:29 -0400

    Force thermometer width to be 100% max

--- views/frontend/widget/peerraiser-campaign-thermometer.php
+++ views/frontend/widget/peerraiser-campaign-thermometer.php
@@ -8,7 +8,7 @@
 
     <div class="peerraiser-thermometer-container">
         <div class="peerraiser-thermometer">
-            <div class="peerraiser-thermometer-bar" style="width: <?php echo $peerraiser['goal_percentage']; ?>%"></div>
+            <div class="peerraiser-thermometer-bar" style="width: <?php echo min( array( $peerraiser['goal_percentage'], 100 ) ); ?>%"></div>
         </div>
         <div class="peerraiser-amount-percentage-info">
 		    <?php printf( wp_kses( __( '<span class="peerraiser-amount-percentage-info-value">%1$s%%</span> <span class="peerraiser-amount-percentage-info-label">Raised of %2$s Goal</span>', 'peerraiser' ), array(  'span' => array( 'class' => array() ) ) ), $peerraiser['goal_percentage'], peerraiser_money_format( $campaign->campaign_goal ) ); ?>
