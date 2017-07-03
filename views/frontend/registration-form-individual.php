516ac0585ca10d31a66c677af985c121ee9e9075
Nate Allen <email@ncallen.com>
2017-07-03 19:59:30 -0400

    Output fields in template

--- views/frontend/registration-form-individual.php
+++ views/frontend/registration-form-individual.php
@@ -1,1 +1,3 @@
-Individual registration form
+<h1><?php echo apply_filters( 'peerraiser_registration_start_team_heading', __( 'Create a Fundraising Page', 'peerraiser' ) ); ?></h1>
+
+<?php echo $fields; ?>
