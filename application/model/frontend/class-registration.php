e3db957d211a121e6d269f430b71f9169399454b
Nate Allen <email@ncallen.com>
2017-07-03 19:14:30 -0400

    Add fields for individual and team

--- application/model/frontend/class-registration.php
+++ application/model/frontend/class-registration.php
@@ -6,6 +6,47 @@ use PeerRaiser\Model\Admin\Admin;
 
 class Registration extends Admin {
 
+	public $fields = array();
+
+	public function __construct() {
+		$this->fields = array(
+			'individual' => array(
+				'fundraising_goal' => array(
+					'name' => __( 'Your Fundraising Goal', 'peerraiser' ),
+					'id'   => 'fundraising_goal',
+					'type' => 'text',
+				),
+				'headline' => array(
+					'name' => __( "Your Page's Headline", 'peerraiser' ),
+					'id' => 'headline',
+					'type' => 'text',
+				),
+				'body' => array(
+					'name' => __( 'Your Story', 'peerraiser' ),
+					'id' => 'body',
+					'type' => 'wysiwyg',
+				),
+			),
+			'add-team' => array(
+				'team_name' => array(
+					'name' => __( 'Your Team Name', 'peerraiser' ),
+					'id'   => 'team_name',
+					'type' => 'text',
+				),
+				'team_goal' => array(
+					'name' => __( 'Team Fundraising Goal', 'peerraiser' ),
+					'id'   => 'team_goal',
+					'type' => 'text',
+				),
+				'headline' => array(
+					'name' => __( "Team's Page Headline", 'peerraiser' ),
+					'id' => 'headline',
+					'type' => 'text',
+				),
+			)
+		);
+	}
+
 	public function get_registration_choices( $campaign ) {
 		$all_choices = array(
 			'individual' => __('Individual', 'peerraiser' ),
@@ -14,6 +55,14 @@ class Registration extends Admin {
 		);
 
 		return $all_choices;
+	}
+
+	public function get_fields( $type = 'all' ) {
+		if ( isset( $this->fields[$type] ) ) {
+			return $this->fields[$type];
+		} else {
+			return $this->fields;
+		}
 	}
 
 }
