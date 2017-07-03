fe1e6226ef627e380dcdab194f165e9bb78fefc6
Nate Allen <email@ncallen.com>
2017-07-03 01:17:27 -0400

    Fix registration button url

--- views/frontend/widget/peerraiser-campaign-register-button.php
+++ views/frontend/widget/peerraiser-campaign-register-button.php
@@ -1,4 +1,4 @@
 <?php
 echo $args['before_widget'];
-printf( '<a href="%1$s?campaign=%2$s">%3$s</a>', $peerraiser['registration_page'], $campaign->campaign_slug, $peerraiser['button_label'] );
+printf( '<a href="%1$s%2$s">%3$s</a>', $peerraiser['registration_page'], $campaign->campaign_slug, $peerraiser['button_label'] );
 echo $args['after_widget'];
