5a98495dd245be817498b2d3aaa4a51a538f902c
Nate Allen <email@ncallen.com>
2017-07-04 01:30:24 -0400

    Redirect to login page if not logged in (when visiting registration page)

--- application/controller/frontend/class-registration.php
+++ application/controller/frontend/class-registration.php
@@ -7,7 +7,28 @@ use PeerRaiser\Controller\Base;
 class Registration extends Base {
 
 	public function register_actions() {
-		add_action( 'cmb2_init', array( $this, 'register_fields') );
+		add_action( 'template_redirect', array( $this, 'registration_redirect' ) );
+		add_action( 'cmb2_init',         array( $this, 'register_fields') );
+	}
+
+	public function registration_redirect() {
+		global $wp_query;
+		$post_id = $wp_query->get_queried_object_id();
+
+		// Get the default dashboard and login page urls
+		$plugin_options    = get_option( 'peerraiser_options', array() );
+		$registration_page = $plugin_options[ 'registration_page' ];
+		$login_page_url    = get_permalink( $plugin_options[ 'login_page' ] );
+
+		// If this is the registration page and the user isn't logged in, redirect to the login page
+		if ( $post_id == $registration_page && ! is_user_logged_in() ) {
+			$args = array(
+				'next_url' => get_permalink( $registration_page )
+			);
+
+			wp_safe_redirect( add_query_arg( $args, $login_page_url ) );
+			exit;
+		}
 	}
 
 	public function register_fields() {
