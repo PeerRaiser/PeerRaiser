d51fafe025f6fd36336033756649fbcb1e2b83c2
Nate Allen <email@ncallen.com>
2017-07-04 13:44:44 -0400

    Add class to choices container

--- views/frontend/partials/registration-choices.php
+++ views/frontend/partials/registration-choices.php
@@ -1,7 +1,7 @@
 <h1><?php echo $headline; ?></h1>
 
 <?php if ( ! empty( $choices ) ) : ?>
-<ul>
+<ul class="peerraiser-fundraising-choices">
 	<?php foreach ( $choices as $key => $value ) : ?>
 		<li class="<?php echo $key; ?>"><a href="./<?php echo $key; ?>"><?php echo $value; ?></a></li>
 	<?php endforeach; ?>
