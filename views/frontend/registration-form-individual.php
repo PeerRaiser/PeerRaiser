9c4b83bc812308f99c26923cf2cc5fa6724d6ba4
Nate Allen <email@ncallen.com>
2017-07-07 00:34:26 -0400

    Add error message container

--- views/frontend/registration-form-individual.php
+++ views/frontend/registration-form-individual.php
@@ -1,3 +1,18 @@
+<?php do_action( 'peerraiser_before_registration_title' ); ?>
+
 <h1><?php echo apply_filters( 'peerraiser_registration_start_team_heading', __( 'Create a Fundraising Page', 'peerraiser' ) ); ?></h1>
 
-<?php echo $fields; ?>
+<?php do_action( 'peerraiser_after_registration_title' ); ?>
+
+<div class="peerraiser-registration-form individual">
+	<?php if ( ! empty( $errors ) ) : ?>
+        <div class="error-message">
+            <strong>Oops!</strong> <?php echo $errors; ?></div>
+	<?php endif; ?>
+
+	<?php do_action( 'peerraiser_before_registration_fields' ); ?>
+
+    <?php echo $fields; ?>
+
+	<?php do_action( 'peerraiser_after_registration_fields' ); ?>
+</div>
