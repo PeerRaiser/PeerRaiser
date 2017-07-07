e759e2c4c624a818bfcc9b2423602e2abe2b67a3
Nate Allen <email@ncallen.com>
2017-07-07 00:31:04 -0400

    Register jquery validate script for the frontend

--- application/controller/frontend/class-post.php
+++ application/controller/frontend/class-post.php
@@ -2,6 +2,8 @@
 
 namespace PeerRaiser\Controller\Frontend;
 
+use PeerRaiser\Core\Setup;
+
 class Post extends \PeerRaiser\Controller\Base {
 
     public function register_actions() {
@@ -21,20 +23,26 @@ class Post extends \PeerRaiser\Controller\Base {
     }
 
     public function add_frontend_scripts() {
+	    wp_register_script(
+		    'jquery-validate',
+		    Setup::get_plugin_config()->get('js_url') . 'vendor/validate/jquery.validate.min.js',
+		    array( 'jquery' ),
+		    '1.16.0',
+		    true
+	    );
         wp_register_script(
             'peerraiser-frontend',
-            \PeerRaiser\Core\Setup::get_plugin_config()->get( 'js_url' ) . 'peerraiser-frontend.js',
-            array( 'jquery' ),
-            \PeerRaiser\Core\Setup::get_plugin_config()->get( 'version' ),
+            Setup::get_plugin_config()->get( 'js_url' ) . 'peerraiser-frontend.js',
+            array( 'jquery', 'jquery-validate' ),
+            Setup::get_plugin_config()->get( 'version' ),
             true
         );
-        $post = get_post();
         wp_localize_script(
             'peerraiser-frontend',
             'peerraiser_variables',
             array(
-                'ajaxUrl'               => admin_url( 'admin-ajax.php' ),
-                'post_id'               => ! empty( $post ) ? $post->ID : false,
+                'ajaxUrl' => admin_url( 'admin-ajax.php' ),
+                'post_id' => ! empty( $post ) ? $post->ID : false,
             )
         );
 
