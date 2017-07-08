3a436030b5aab1cd08ff002850207f2e84bb0fd7
Nate Allen <email@ncallen.com>
2017-07-08 12:01:47 -0400

    Add logic for registering individual

--- application/controller/frontend/class-registration.php
+++ application/controller/frontend/class-registration.php
@@ -3,6 +3,10 @@
 namespace PeerRaiser\Controller\Frontend;
 
 use PeerRaiser\Controller\Base;
+use PeerRaiser\Helper\File;
+use PeerRaiser\Model\Campaign;
+use PeerRaiser\Model\Fundraiser;
+use PeerRaiser\Model\Participant;
 
 class Registration extends Base {
 
@@ -53,6 +57,11 @@ class Registration extends Base {
 
 	public function register_individual() {
 		$registration_model = new \PeerRaiser\Model\Frontend\Registration();
+		$fundraiser         = new Fundraiser();
+		$participant_model  = new Participant();
+		$campaign_model     = new Campaign();
+		$campaign           = $campaign_model->get_campaigns( array( 'slug' => get_query_var( 'peerraiser_campaign' ) ) );
+		$participant        = $participant_model->get_current_participant();
 
 		// If no form submission, bail
 		if ( empty( $_POST ) || ! isset( $_POST['submit-cmb'], $_POST['object_id'] ) ) {
@@ -64,7 +73,12 @@ class Registration extends Base {
 
 		// Check security nonce
 		if ( ! isset( $_POST[ $cmb->nonce() ] ) || ! wp_verify_nonce( $_POST[ $cmb->nonce() ], $cmb->nonce() ) ) {
-			return $cmb->prop( 'submission_error', newWP_Error( 'security_fail', __( 'Security check failed.' ) ) );
+			return $cmb->prop( 'submission_error', new WP_Error( 'security_fail', __( 'Security check failed.' ) ) );
+		}
+
+		// Make sure a campaign is specified
+		if ( empty( $campaign ) ) {
+			return $cmb->prop( 'submission_error', new \WP_Error( 'peerraiser_campaign_missing', __( 'A campaign is required. No campaign specified.' ) ) );
 		}
 
 		$required_fields = $registration_model->get_required_field_ids( 'individual' );
@@ -76,8 +90,48 @@ class Registration extends Base {
 			}
 		}
 
+		// TODO: Check if file type and size is allowed
+		// TODO: Check if goal amount is correct format
+		// TODO: Check if campaign accepting fundraisers
+
 		if ( ! empty( $errors ) ) {
-			return $cmb->prop( 'submission_error', new \WP_Error( 'post_data_missing', __( 'Some required fields are empty' ) ) );
+			return $cmb->prop( 'submission_error', new \WP_Error( 'post_data_missing', __( 'Some required fields are empty.' ) ) );
 		}
+
+		/**
+		 * Fetch sanitized values
+		 */
+		$sanitized_values = $cmb->get_sanitized_values( $_POST );
+
+		$fundraiser->fundraiser_name    = $sanitized_values['_peerraiser_headline'];
+		$fundraiser->fundraiser_slug    = sanitize_title( $participant->full_name );
+		$fundraiser->fundraiser_content = $sanitized_values['_peerraiser_body'];
+		$fundraiser->campaign_id        = $campaign[0]->ID;
+		$fundraiser->participant        = $participant->ID;
+
+		// Unset data that shouldn't be saved as post meta
+		unset( $sanitized_values['_peerraiser_headline'] );
+		unset( $sanitized_values['_peerraiser_body'] );
+		unset( $sanitized_values['peerraiser_action'] );
+
+		$fundraiser->save();
+
+		foreach ( $sanitized_values as $key => $value ) {
+			$fundraiser->update_meta( $key, $value );
+		}
+
+		error_log( print_r( $fundraiser,1 ) );
+
+		// Try to upload the featured image
+		$image_id = File::attach_image_to_post( $fundraiser->ID );
+
+		// If image upload was successful, set the featured image
+		if ( $image_id && ! is_wp_error( $image_id ) ) {
+			set_post_thumbnail( $fundraiser->ID, $image_id );
+		}
+
+		// Redirect to the new fundraiser
+		wp_safe_redirect( get_permalink( $fundraiser->ID ) );
+		exit;
 	}
 }
