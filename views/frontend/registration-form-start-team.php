2282c27a189ed507d6a5f072e4c9ca2ef0c53737
Nate Allen <email@ncallen.com>
2017-07-08 18:56:04 -0400

    Add container around fields

--- views/frontend/registration-form-start-team.php
+++ views/frontend/registration-form-start-team.php
@@ -1,3 +1,18 @@
+<?php do_action( 'peerraiser_before_registration_title' ); ?>
+
 <h1><?php echo apply_filters( 'peerraiser_registration_start_team_heading', __( 'Start a Team', 'peerraiser' ) ); ?></h1>
 
-<?php echo $fields; ?>
+<?php do_action( 'peerraiser_after_registration_title' ); ?>
+
+<div class="peerraiser-registration-form start-team">
+	<?php if ( ! empty( $errors ) ) : ?>
+        <div class="error-message">
+            <strong><?php _e( 'Oops!', 'peerraiser' ) ?></strong> <?php echo $errors; ?></div>
+	<?php endif; ?>
+
+	<?php do_action( 'peerraiser_before_registration_fields' ); ?>
+
+	<?php echo $fields; ?>
+
+	<?php do_action( 'peerraiser_after_registration_fields' ); ?>
+</div>
