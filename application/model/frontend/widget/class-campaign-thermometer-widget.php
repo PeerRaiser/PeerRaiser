21a2cdfca50c402b1b0bd74bdd7720bcf29bec99
Nate Allen <email@ncallen.com>
2017-07-16 15:08:14 -0400

    Allow manual selection for campaign

--- application/model/frontend/widget/class-campaign-thermometer-widget.php
+++ application/model/frontend/widget/class-campaign-thermometer-widget.php
@@ -2,6 +2,8 @@
 
 namespace PeerRaiser\Model\Frontend\Widget;
 
+use PeerRaiser\Model\Campaign;
+
 class Campaign_Thermometer_Widget extends PeerRaiser_Widget {
 
 	public function __construct() {
@@ -15,13 +17,13 @@ class Campaign_Thermometer_Widget extends PeerRaiser_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		$plugin_options = get_option( 'peerraiser_options', array() );
-		$campaign = peerraiser_get_current_campaign();
+		if ( $instance['campaign'] === 'auto' || empty( $instance['campaign'] ) ) {
+			$campaign = peerraiser_get_current_campaign();
+		} else {
+			$campaign = new Campaign( $instance['campaign']);
+		}
 
-		$view_args = array(
-			'goal_percentage' => round( ( $campaign->donation_value / $campaign->campaign_goal) * 100 )
-		);
-		$this->assign( 'peerraiser', $view_args );
+		$this->assign( 'goal_percentage', round( ( $campaign->donation_value / $campaign->campaign_goal) * 100 ) );
 		$this->assign( 'campaign', $campaign );
 		$this->assign( 'args', $args );
 		$this->assign( 'instance', $instance );
@@ -30,8 +32,12 @@ class Campaign_Thermometer_Widget extends PeerRaiser_Widget {
 	}
 
 	public function form( $instance ) {
+		$campaign_model = new Campaign();
+
 		$view_args = array(
 			'title' => ! empty( $instance['title'] ) ? $instance['title'] : '',
+			'campaign' => ! empty( $instance['campaign'] ) ? $instance['campaign'] : 'auto',
+			'campaigns' => $campaign_model->get_campaigns(),
 		);
 		$this->assign( 'peerraiser', $view_args );
 
@@ -41,6 +47,7 @@ class Campaign_Thermometer_Widget extends PeerRaiser_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ! empty( $new_instance['title'] ) ? esc_attr( $new_instance['title'] ) : '';
+		$instance['campaign'] = ! empty( $new_instance['campaign'] ) ? esc_attr( $new_instance['campaign'] ) : 'auto';
 
 		return $instance;
 	}
