c7683e257929653cd85ca70c2106d50668556c4e
Nate Allen <email@ncallen.com>
2017-07-15 23:09:37 -0400

    Add fundraiser template

---
+++ views/frontend/template-peerraiser-fundraiser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the PeerRaiser Campaign
+ */
+$fundraiser = peerraiser_get_current_fundraiser();
+?>
+
+<?php get_header(); ?>
+
+<div id="peerraiser-fundraiser" class="<?php echo 'peerraiser-fundraiser-' . $fundraiser->ID ?> <?php echo 'peerraiser-fundraiser-' . $fundraiser->fundraiser_slug; ?>">
+
+    <div class="peerraiser-fundraiser-content<?php if ( is_active_sidebar( 'peerraiser-fundraiser-sidebar' ) ) echo ' has-sidebar'; ?>">
+
+        <h1 class="peerraiser-fundraiser-title"><?php echo $fundraiser->fundraiser_name; ?></h1>
+
+        <div class="peerraiser-fundraiser-description">
+		    <?php echo $fundraiser->fundraiser_content; ?>
+        </div>
+
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
