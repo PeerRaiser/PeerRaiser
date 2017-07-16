378be98f71173e35a24437299a4eff68c4cd314f
Nate Allen <email@ncallen.com>
2017-07-16 15:09:07 -0400

    Make  its own variable

--- views/frontend/widget/peerraiser-campaign-thermometer.php
+++ views/frontend/widget/peerraiser-campaign-thermometer.php
@@ -8,10 +8,10 @@
 
     <div class="peerraiser-thermometer-container">
         <div class="peerraiser-thermometer">
-            <div class="peerraiser-thermometer-bar" style="width: <?php echo min( array( $peerraiser['goal_percentage'], 100 ) ); ?>%"></div>
+            <div class="peerraiser-thermometer-bar" style="width: <?php echo min( array( $goal_percentage, 100 ) ); ?>%"></div>
         </div>
         <div class="peerraiser-amount-percentage-info">
-		    <?php printf( wp_kses( __( '<span class="peerraiser-amount-percentage-info-value">%1$s%%</span> <span class="peerraiser-amount-percentage-info-label">Raised of %2$s Goal</span>', 'peerraiser' ), array(  'span' => array( 'class' => array() ) ) ), $peerraiser['goal_percentage'], peerraiser_money_format( $campaign->campaign_goal ) ); ?>
+		    <?php printf( wp_kses( __( '<span class="peerraiser-amount-percentage-info-value">%1$s%%</span> <span class="peerraiser-amount-percentage-info-label">Raised of %2$s Goal</span>', 'peerraiser' ), array(  'span' => array( 'class' => array() ) ) ), $goal_percentage, peerraiser_money_format( $campaign->campaign_goal ) ); ?>
         </div>
     </div>
 
