660af1b2064e7a28aed2cb87899458e0e906baaf
Nate Allen <email@ncallen.com>
2017-07-16 14:08:08 -0400

    Move  logic to the model

--- views/frontend/widget/peerraiser-top-teams.php
+++ views/frontend/widget/peerraiser-top-teams.php
@@ -1,6 +1,5 @@
 <?php echo $args['before_widget']; ?>
 
-<?php $top_teams = peerraiser_get_top_teams( $instance['list_size'], array( 'campaign_id' => $campaign->ID ) ); ?>
 <?php if ( ! empty( $instance['title'] ) ) : ?>
     <?php echo $args['before_title']; ?>
         <?php echo $instance['title']; ?>
