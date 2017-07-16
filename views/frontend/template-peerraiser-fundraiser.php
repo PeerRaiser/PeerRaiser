dca4b92477a623b1362c8bee3503d0600271bfe6
Nate Allen <email@ncallen.com>
2017-07-15 23:23:16 -0400

    Add sidebar

--- views/frontend/template-peerraiser-fundraiser.php
+++ views/frontend/template-peerraiser-fundraiser.php
@@ -9,6 +9,16 @@ $fundraiser = peerraiser_get_current_fundraiser();
 
 <div id="peerraiser-fundraiser" class="<?php echo 'peerraiser-fundraiser-' . $fundraiser->ID ?> <?php echo 'peerraiser-fundraiser-' . $fundraiser->fundraiser_slug; ?>">
 
+	<?php if ( is_active_sidebar( 'peerraiser-fundraiser-sidebar' ) ) : ?>
+
+        <div class="peerraiser-fundraiser-sidebar">
+
+			<?php dynamic_sidebar( 'peerraiser-fundraiser-sidebar' ); ?>
+
+        </div>
+
+	<?php endif; ?>
+
     <div class="peerraiser-fundraiser-content<?php if ( is_active_sidebar( 'peerraiser-fundraiser-sidebar' ) ) echo ' has-sidebar'; ?>">
 
         <h1 class="peerraiser-fundraiser-title"><?php echo $fundraiser->fundraiser_name; ?></h1>
