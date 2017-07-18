25caa3772e0a37ed4f9743430157e991e82d05d1
Nate Allen <email@ncallen.com>
2017-07-18 01:04:28 -0400

    Add field to select campaign

--- views/backend/widget/peerraiser-campaign-total-raised.php
+++ views/backend/widget/peerraiser-campaign-total-raised.php
@@ -7,6 +7,15 @@
     <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'after_amount' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'after_amount' ) ); ?>" type="text" value="<?php echo esc_attr( $peerraiser['after_amount'] ); ?>" placeholder="<?php _e( '', 'peerraiser'); ?>">
 </p>
 <p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>"><?php esc_attr_e( 'Campaign:', 'peerraiser' ); ?></label>
+    <select name="<?php echo esc_attr( $this->get_field_name( 'campaign' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>">
+        <option value="auto" <?php selected( $peerraiser['campaign'], 'auto' ); ?>><?php _e( 'Current Campaign (auto detect)', 'peerraiser' ); ?></option>
+		<?php foreach ( $peerraiser['campaigns'] as $campaign ) :?>
+            <option value="<?php echo esc_attr( $campaign->ID ); ?>" <?php selected( absint( $peerraiser['campaign'] ), $campaign->ID ) ?>><?php echo $campaign->campaign_name; ?></option>
+		<?php endforeach; ?>
+    </select>
+</p>
+<p>
     <input class="checkbox" type="checkbox" <?php checked( $peerraiser[ 'hide_if_zero' ], 'on' ); ?> id="<?php echo $this->get_field_id( 'hide_if_zero' ); ?>" name="<?php echo $this->get_field_name( 'hide_if_zero' ); ?>" />
     <label for="<?php echo $this->get_field_id( 'hide_if_zero' ); ?>"><?php _e( 'Hide this widget if total raised is 0', 'peerraiser' ); ?></label>
 </p>
