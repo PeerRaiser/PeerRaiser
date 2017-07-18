135e370cc92dacb247d04d5d19f715917a0e8813
Nate Allen <email@ncallen.com>
2017-07-18 01:04:28 -0400

    Add field to select campaign

--- views/backend/widget/peerraiser-campaign-donate-button.php
+++ views/backend/widget/peerraiser-campaign-donate-button.php
@@ -2,3 +2,12 @@
 	<label for="<?php echo esc_attr( $this->get_field_id( 'button_label' ) ); ?>"><?php esc_attr_e( 'Button Title:', 'peerraiser' ); ?></label>
 	<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'button_label' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'button_label' ) ); ?>" type="text" value="<?php echo esc_attr( $peerraiser['button_label'] ); ?>" placeholder="<?php _e( 'i.e. Donate to this campaign', 'peerraiser'); ?>">
 </p>
+<p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>"><?php esc_attr_e( 'Campaign:', 'peerraiser' ); ?></label>
+    <select name="<?php echo esc_attr( $this->get_field_name( 'campaign' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>">
+        <option value="auto" <?php selected( $peerraiser['campaign'], 'auto' ); ?>><?php _e( 'Current Campaign (auto detect)', 'peerraiser' ); ?></option>
+		<?php foreach ( $peerraiser['campaigns'] as $campaign ) :?>
+            <option value="<?php echo esc_attr( $campaign->ID ); ?>" <?php selected( absint( $peerraiser['campaign'] ), $campaign->ID ) ?>><?php echo $campaign->campaign_name; ?></option>
+		<?php endforeach; ?>
+    </select>
+</p>
