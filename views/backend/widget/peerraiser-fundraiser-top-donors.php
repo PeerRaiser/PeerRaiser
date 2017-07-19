91431ef5c013d00f52d12c5044f2e769f3897d42
Nate Allen <email@ncallen.com>
2017-07-19 06:47:45 -0400

    Replace 'campaign' with 'fundraiser'

--- views/backend/widget/peerraiser-fundraiser-top-donors.php
+++ views/backend/widget/peerraiser-fundraiser-top-donors.php
@@ -7,12 +7,11 @@
     <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'list_size' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'list_size' ) ); ?>" type="number" value="<?php echo esc_attr( $peerraiser['list_size'] ); ?>">
 </p>
 <p>
-    <label for="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>"><?php esc_attr_e( 'Campaign:', 'peerraiser' ); ?></label>
-    <select name="<?php echo esc_attr( $this->get_field_name( 'campaign' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'campaign' ) ); ?>">
-        <option value="auto" <?php selected( $peerraiser['campaign'], 'auto' ); ?>><?php _e( 'Current Campaign (auto detect)', 'peerraiser' ); ?></option>
-        <option value="all" <?php selected( $peerraiser['campaign'], 'all' ); ?>><?php _e( 'All Campaigns', 'peerraiser' ); ?></option>
-		<?php foreach ( $peerraiser['campaigns'] as $campaign ) :?>
-            <option value="<?php echo esc_attr( $campaign->ID ); ?>" <?php selected( absint( $peerraiser['campaign'] ), $campaign->ID ) ?>><?php echo $campaign->campaign_name; ?></option>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'fundraiser' ) ); ?>"><?php esc_attr_e( 'Fundraiser:', 'peerraiser' ); ?></label>
+    <select name="<?php echo esc_attr( $this->get_field_name( 'fundraiser' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'fundraiser' ) ); ?>">
+        <option value="auto" <?php selected( $peerraiser['fundraiser'], 'auto' ); ?>><?php _e( 'Current Fundraiser (auto detect)', 'peerraiser' ); ?></option>
+		<?php foreach ( $peerraiser['fundraisers'] as $fundraiser ) :?>
+            <option value="<?php echo esc_attr( $fundraiser->ID ); ?>" <?php selected( absint( $peerraiser['fundraiser'] ), $fundraiser->ID ) ?>><?php echo $fundraiser->fundraiser_name; ?> (#<?php echo $fundraiser->ID; ?>)</option>
 		<?php endforeach; ?>
     </select>
 </p>
