bd8ff25eec3246c50e739977889d6bbf4061ab1d
Nate Allen <email@ncallen.com>
2017-07-22 13:33:58 -0400

    Force is_test to always be 0, so test donations don't show in list

--- application/model/database/class-donor-table.php
+++ application/model/database/class-donor-table.php
@@ -224,10 +224,6 @@ class Donor_Table extends Database {
     public function get_top_donors_to_fundraiser( $id, $count = 20 ) {
 	    global $wpdb;
 
-	    $plugin_options = get_option( 'peerraiser_options', array() );
-
-	    $is_test = filter_var( $plugin_options['test_mode'], FILTER_VALIDATE_BOOLEAN ) ? 1 : 0;
-
 	    $results = $wpdb->get_results(
 		    $wpdb->prepare(
 		    	"SELECT pr_donations.donor_id, sum(pr_donations.total) as total, pr_donors.full_name
@@ -236,11 +232,11 @@ class Donor_Table extends Database {
 				ON pr_donors.donor_id = pr_donations.donor_id
 				WHERE pr_donations.fundraiser_id = %d
 				AND pr_donations.status = 'completed'  
-				AND pr_donations.is_test = %d
+				AND pr_donations.is_test = 0
 				GROUP BY pr_donations.donor_id
 				ORDER BY total DESC
 				LIMIT %d",
-			    absint( $id ), absint( $is_test ), absint( $count )
+			    absint( $id ), absint( $count )
 		    )
 	    );
 
@@ -250,23 +246,19 @@ class Donor_Table extends Database {
 	public function get_top_donors_to_campaign( $id, $count = 20 ) {
 		global $wpdb;
 
-		$plugin_options = get_option( 'peerraiser_options', array() );
-
-		$is_test = filter_var( $plugin_options['test_mode'], FILTER_VALIDATE_BOOLEAN ) ? 1 : 0;
-
 		$results = $wpdb->get_results(
 			$wpdb->prepare(
 				"SELECT pr_donations.donor_id, sum(pr_donations.total) as total, pr_donors.full_name
 				FROM {$wpdb->prefix}pr_donations as pr_donations
 				INNER JOIN {$wpdb->prefix}pr_donors as pr_donors
 				ON pr_donors.donor_id = pr_donations.donor_id
-				WHERE pr_donations.campaign_id = %s 
+				WHERE pr_donations.campaign_id = %d 
 				AND pr_donations.status = 'completed'  
-				AND pr_donations.is_test = %s 
+				AND pr_donations.is_test = 0
 				GROUP BY pr_donations.donor_id
 				ORDER BY total DESC
-				LIMIT %s",
-				absint( $id, $is_test, $count )
+				LIMIT %d",
+				absint( $id ), absint( $count )
 			)
 		);
 
