37194c2e8b1774a99e1ed26e45ed9ee95cfa3019
Nate Allen <email@ncallen.com>
2017-07-23 14:17:28 -0400

    Output the top donors

--- views/frontend/widget/peerraiser-fundraiser-top-donors.php
+++ views/frontend/widget/peerraiser-fundraiser-top-donors.php
@@ -5,10 +5,11 @@
         <?php echo $instance['title']; ?>
     <?php echo $args['after_title']; ?>
 <?php endif; ?>
+
 <?php if ( ! empty( $top_donors ) ):  ?>
     <ol>
         <?php foreach ( $top_donors as $donor ) : ?>
-            <li><a href="<?php echo get_the_permalink( $donor->ID ); ?>"><?php echo $donor->full_name; ?></a> (<?php echo peerraiser_money_format( $donor->donation_value ); ?>)</li>
+            <li><?php echo $donor->full_name; ?> (<?php echo peerraiser_money_format( $donor->total ); ?>)</li>
         <?php endforeach; ?>
     </ol>
 <?php endif; ?>
