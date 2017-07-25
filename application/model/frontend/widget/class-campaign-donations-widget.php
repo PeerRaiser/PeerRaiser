f1f116da9386ae54f338f0fc4b8298cc90089d35
Nate Allen <email@ncallen.com>
2017-07-24 23:09:52 -0400

    Change top donors widget to donations

--- application/model/frontend/widget/class-campaign-donations-widget.php
+++ application/model/frontend/widget/class-campaign-donations-widget.php
@@ -3,49 +3,53 @@
 namespace PeerRaiser\Model\Frontend\Widget;
 
 use PeerRaiser\Model\Campaign;
+use PeerRaiser\Model\Donation;
 
-class Campaign_Top_Donors_Widget extends PeerRaiser_Widget {
+class Campaign_Donations_Widget extends PeerRaiser_Widget {
 
 	public function __construct() {
-		$widget_title = apply_filters( 'peerraiser-campaign-top-donors-widget-title', __( 'Campaign Top Donors', 'peerraiser' ) );
-		$widget_options = apply_filters( 'peerraiser-campaign-top-donors-widget-options', array(
-			'classname' => 'peerraiser-campaign-top-donors',
-			'description' => __( 'Display a list of the top donors by campaign (or all)', 'peerraiser' ),
+		$widget_title = apply_filters( 'peerraiser-campaign-donations-widget-title', __( 'Campaign Donations', 'peerraiser' ) );
+		$widget_options = apply_filters( 'peerraiser-campaign-donations-widget-options', array(
+			'classname' => 'peerraiser-campaign-donations',
+			'description' => __( 'Display a list of donations to a campaign (or all campaigns)', 'peerraiser' ),
 		) );
 
-		parent::__construct('peerraiser_campaign_top_donors', $widget_title, $widget_options );
+		parent::__construct('peerraiser_campaign_donations', $widget_title, $widget_options );
 	}
 
 	public function widget( $args, $instance ) {
+		$donation_model = new Donation();
+
 		if ( $instance['campaign'] === 'auto' ) {
 			$campaign = peerraiser_get_current_campaign();
-			$top_donors = peerraiser_get_top_donors( $instance['list_size'], array( 'campaign_id' => $campaign->ID ) );
+			$donations = $donation_model->get_donations( array( 'campaign_id' => $campaign->ID ) );
 		} elseif( $instance['campaign'] == 'all' || empty( $instance['campaign'] ) ) {
-			$top_donors = peerraiser_get_top_donors( $instance['list_size'] );
+			$donations = $donation_model->get_donations();
+			$donations = peerraiser_get_donations( $instance['list_size'] );
 		} else {
 			$campaign = new Campaign( $instance['campaign']);
-			$top_donors = peerraiser_get_top_donors( $instance['list_size'], array( 'campaign_id' => $campaign->ID ) );
+			$donations = peerraiser_get_donations( $instance['list_size'], array( 'campaign_id' => $campaign->ID ) );
 		}
 
 		$this->assign( 'args', $args );
-		$this->assign( 'top_donors', $top_donors );
+		$this->assign( 'donations', $donations );
 		$this->assign( 'instance', $instance );
 
-		echo $this->get_text_view( 'frontend/widget/peerraiser-campaign-top-donors' );
+		echo $this->get_text_view( 'frontend/widget/peerraiser-campaign-donations' );
 	}
 
 	public function form( $instance ) {
 		$campaign_model = new Campaign();
 
 		$view_args = array(
-			'title' => ! empty( $instance['title'] ) ? $instance['title'] : __( 'Top Donors', 'peerraiser' ),
+			'title' => ! empty( $instance['title'] ) ? $instance['title'] : __( 'Donations', 'peerraiser' ),
 			'list_size' => ! empty( $instance['list_size'] ) ? $instance['list_size'] : 10,
 			'campaign' => ! empty( $instance['campaign'] ) ? $instance['campaign'] : 'auto',
 			'campaigns' => $campaign_model->get_campaigns(),
 		);
 		$this->assign( 'peerraiser', $view_args );
 
-		echo $this->get_text_view( 'backend/widget/peerraiser-campaign-top-donors' );
+		echo $this->get_text_view( 'backend/widget/peerraiser-campaign-donations' );
 	}
 
 	public function update( $new_instance, $old_instance ) {
