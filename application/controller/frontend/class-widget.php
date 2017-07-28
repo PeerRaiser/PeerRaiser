fbf7c0b5683e72384c8274e316a1fd751afae302
Nate Allen <email@ncallen.com>
2017-07-27 22:18:39 -0400

    Register widgets for fundraiser donation button and thermometer

--- application/controller/frontend/class-widget.php
+++ application/controller/frontend/class-widget.php
@@ -47,10 +47,9 @@ class Widget extends Base {
 
 		// Fundraiser Widgets
 		register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Donations_Widget' );
-		//register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Donate_Button_Widget' );
+		register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Donate_Button_Widget' );
 		register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Total_Raised_Widget' );
-		//register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Thermometer_Widget' );
-		//register_widget( 'PeerRaiser\Model\Frontend\Widget\Top_Donors_Widget' );
+		register_widget( 'PeerRaiser\Model\Frontend\Widget\Fundraiser_Thermometer_Widget' );
 	}
 
 }
