022a5f187c87bd9646460fcc137d7fecae70fcda
Nate Allen <email@ncallen.com>
2017-07-29 17:06:25 -0400

    Display teams

--- views/frontend/participant-dashboard-profile.php
+++ views/frontend/participant-dashboard-profile.php
@@ -12,13 +12,10 @@
     <?php if ( ! empty( $peerraiser['fundraisers'] ) ) : ?>
         <div class="fundraisers">
             <?php foreach( $peerraiser['fundraisers'] as $fundraiser ) : ?>
-                <?php
-                $campaign = new \PeerRaiser\Model\Campaign( $fundraiser->campaign_id );
-                ?>
+                <?php $campaign = new \PeerRaiser\Model\Campaign( $fundraiser->campaign_id ); ?>
 
                 <div class="fundraiser">
                     <div class="thumbnail">
-                        <?php error_log( 'image: ' . $campaign->thumbnail_image ); ?>
                         <img src="<?php echo $campaign->thumbnail_image; ?>">
                     </div>
                     <div class="content">
@@ -36,28 +33,26 @@
         <p><?php _e( 'You do not currently have any fundraisers', 'peerraiser' ); ?></p>
     <?php endif; ?>
 
-    <?php if ( $peerraiser['teams']->have_posts() ) : ?>
+    <?php if ( ! empty( $peerraiser['teams'] ) ) : ?>
         <h2><?php _e( 'My Teams', 'peerraiser' ); ?></h2>
         <div class="teams">
-        <?php while ( $peerraiser['teams']->have_posts() ) : $peerraiser['teams']->the_post() ?>
+        <?php foreach ( $peerraiser['teams'] as $team ) : ?>
+	        <?php $campaign = new \PeerRaiser\Model\Campaign( $team->campaign_id ); ?>
+
             <div class="team">
                 <div class="thumbnail">
-                    <?php $thumbnail = get_post_meta( get_the_ID(), '_peerraiser_team_thumbnail', true); ?>
-                    <?php $image = wp_get_attachment_image_src( get_post_meta( get_the_ID(), '_peerraiser_team_thumbnail_id', 1 ), 'peerraiser_team_thumbnail' ); ?>
-                    <?php $thumbnail = ( !empty($image) ) ? $image[0] : $peerraiser['default_team_thumbnail'] ?>
-                    <img src="<?php echo $thumbnail ?>">
+                    <img src="<?php echo $team->thumbnail_image; ?>">
                 </div>
                 <div class="content">
-                    <?php $campaign_id = get_post_meta( get_the_ID(), '_team_campaign', true ) ?>
-                    <h3><?php the_title(); ?></h3>
-                    <p><?php echo get_the_title( $campaign_id ) ?></p>
+                    <h3><?php echo esc_attr( $team->team_name ); ?></h3>
+                    <p><?php echo esc_attr( $campaign->campaign_name ); ?></p>
                 </div>
                 <div class="controls">
-                    <a href="<?php echo add_query_arg( 'edit', 'team', get_the_permalink() ) ?>"><?php _e( 'Manage', 'peerraiser' ); ?></a>
-                    <a href="<?php echo get_the_permalink() ?>"><?php _e( 'View', 'peerraiser' ); ?></a>
+                    <!--<a href="--><?php //echo add_query_arg( 'edit', 'team', get_the_permalink() ) ?><!--">--><?php //_e( 'Manage', 'peerraiser' ); ?><!--</a>-->
+                    <a href="<?php echo $team->get_permalink() ?>"><?php _e( 'View', 'peerraiser' ); ?></a>
                 </div>
             </div>
-        <?php endwhile; ?>
+        <?php endforeach; ?>
         </div>
     <?php endif; ?>
 
