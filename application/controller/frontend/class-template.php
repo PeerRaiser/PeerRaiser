df95165dbecee6214615a1ec8908e62b9dd335e8
Nate Allen <email@ncallen.com>
2017-07-30 00:08:24 -0400

    Handle loading team or campaign template

--- application/controller/frontend/class-template.php
+++ application/controller/frontend/class-template.php
@@ -5,19 +5,39 @@ namespace PeerRaiser\Controller\Frontend;
 class Template extends \PeerRaiser\Controller\Base {
 
 	public function register_actions() {
-		add_filter( 'archive_template', array( $this, 'load_campaign_template' ) );
+		add_filter( 'archive_template', array( $this, 'load_template' ) );
 	}
 
-	public function load_campaign_template( $template ) {
+	public function load_template( $template ) {
 		$term = get_queried_object();
 
-		// If this isn't the peerraiser campaign, return default template
-		if ( $term->taxonomy !== "peerraiser_campaign" ) {
-			return $template;
+		if ( $term->taxonomy === 'peerraiser_campaign' ) {
+			return $this->load_campaign_template( $term );
+		} elseif ( $term->taxonomy === 'peerraiser_team' ) {
+			return $this->load_team_template( $term );
 		}
 
+		return $template;
+	}
+
+	public function load_campaign_template( $term ) {
 		$specific_template_file = 'template-peerraiser-campaign-' . $term->slug . '.php';
 		$general_template_file  = 'template-peerraiser-campaign.php';
+
+		if ( $specific_theme_file = locate_template( array( $specific_template_file ) ) ) {
+			$template = $specific_theme_file;
+		} elseif ( $general_theme_file = locate_template( array ( $general_template_file ) ) ) {
+			$template = $general_theme_file;
+		} else {
+			$template = PEERRAISER_PATH . 'views/frontend/' . $general_template_file;
+		}
+
+		return $template;
+	}
+
+	public function load_team_template( $term ) {
+		$specific_template_file = 'template-peerraiser-team-' . $term->slug . '.php';
+		$general_template_file  = 'template-peerraiser-team.php';
 
 		if ( $specific_theme_file = locate_template( array( $specific_template_file ) ) ) {
 			$template = $specific_theme_file;
