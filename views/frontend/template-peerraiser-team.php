d680c32af6878a4f0e8150d3c2bbd5b159ccdc3c
Nate Allen <email@ncallen.com>
2017-07-30 22:55:33 -0400

    Escape attributes before echoing

--- views/frontend/template-peerraiser-team.php
+++ views/frontend/template-peerraiser-team.php
@@ -1,31 +1,33 @@
 <?php
 /**
- * The template for displaying the PeerRaiser Team
+ * The template for displaying a PeerRaiser Team
  */
 $team = peerraiser_get_current_team();
 ?>
 
 <?php get_header(); ?>
 
-<div id="peerraiser-team" class="<?php echo 'peerraiser-team-' . $team->ID ?> <?php echo 'peerraiser-team-' . $team->team_slug; ?>">
+<div id="peerraiser-team" class="<?php echo 'peerraiser-team-' . $team->ID ?> <?php echo 'peerraiser-team-' . esc_attr( $team->team_slug ); ?>">
 
-    <div class="peerraiser-team-sidebar">
+    <div class="peerraiser-team-content<?php if ( is_active_sidebar( 'peerraiser-team-sidebar' ) ) echo ' has-sidebar'; ?>">
 
-        <img src="<?php echo esc_url( $team->thumbnail_image ) ?>" alt="<?php _e( 'Team picture', 'peerraiser' ); ?>">
-
-        <?php //dynamic_sidebar( 'peerraiser-team-sidebar' ); ?>
-
-    </div>
-
-    <div class="peerraiser-team-content has-sidebar">
-
-        <h1 class="peerraiser-team-title"><?php echo $team->team_name; ?></h1>
+        <h1 class="peerraiser-team-title"><?php echo esc_attr( $team->team_name ); ?></h1>
 
         <div class="peerraiser-team-description">
-            <?php echo $team->team_content; ?>
+            <?php echo wp_kses_post( $team->team_content ); ?>
         </div>
 
     </div>
+
+    <?php if ( is_active_sidebar( 'peerraiser-team-sidebar' ) ) : ?>
+
+        <div class="peerraiser-team-sidebar">
+
+            <?php dynamic_sidebar( 'peerraiser-team-sidebar' ); ?>
+
+        </div>
+
+    <?php endif; ?>
 
 </div>
 
