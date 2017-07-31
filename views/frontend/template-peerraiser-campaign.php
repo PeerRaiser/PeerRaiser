cf377dbef22d786f324a4e7f94ff01868fb6cb7e
Nate Allen <email@ncallen.com>
2017-07-30 22:55:19 -0400

    Escape attributes before echoing

--- views/frontend/template-peerraiser-campaign.php
+++ views/frontend/template-peerraiser-campaign.php
@@ -7,20 +7,20 @@ $campaign = peerraiser_get_current_campaign();
 
 <?php get_header(); ?>
 
-<div id="peerraiser-campaign" class="<?php echo 'peerraiser-campaign-' . $campaign->ID ?> <?php echo 'peerraiser-campaign-' . $campaign->campaign_slug; ?>">
+<div id="peerraiser-campaign" class="<?php echo 'peerraiser-campaign-' . $campaign->ID ?> <?php echo 'peerraiser-campaign-' . esc_attr( $campaign->campaign_slug ); ?>">
 
     <div class="peerraiser-campaign-content<?php if ( is_active_sidebar( 'peerraiser-campaign-sidebar' ) ) echo ' has-sidebar'; ?>">
 
         <?php if ( $campaign->banner_image ) : ?>
             <div class="peerraiser-campaign-banner">
-                <img src="<?php echo $campaign->banner_image; ?>" alt="<?php echo $campaign->campaign_name; ?>">
+                <img src="<?php echo esc_url( $campaign->banner_image ); ?>" alt="<?php echo esc_attr( $campaign->campaign_name ); ?>">
             </div>
         <?php endif; ?>
 
-        <h1 class="peerraiser-campaign-title"><?php echo $campaign->campaign_name; ?></h1>
+        <h1 class="peerraiser-campaign-title"><?php echo esc_attr( $campaign->campaign_name ); ?></h1>
 
         <div class="peerraiser-campaign-description">
-            <?php echo $campaign->campaign_description; ?>
+            <?php echo wp_kses_post( $campaign->campaign_description ); ?>
         </div>
 
     </div>
