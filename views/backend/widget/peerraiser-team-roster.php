5f4556a4a6479362012ab5cb809d77191eae91ab
Nate Allen <email@ncallen.com>
2017-08-03 22:22:16 -0400

    Add team roster widget backend template

---
+++ views/backend/widget/peerraiser-team-roster.php
@@ -0,0 +1,17 @@
+<p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'peerraiser' ); ?></label>
+    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $peerraiser['title'] ); ?>">
+</p>
+<p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'list_size' ) ); ?>"><?php esc_attr_e( 'List Size:', 'peerraiser' ); ?></label>
+    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'list_size' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'list_size' ) ); ?>" type="number" value="<?php echo esc_attr( $peerraiser['list_size'] ); ?>">
+</p>
+<p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'team' ) ); ?>"><?php esc_attr_e( 'Team:', 'peerraiser' ); ?></label>
+    <select name="<?php echo esc_attr( $this->get_field_name( 'team' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'team' ) ); ?>">
+        <option value="auto" <?php selected( $peerraiser['team'], 'auto' ); ?>><?php _e( 'Current Team (auto detect)', 'peerraiser' ); ?></option>
+        <?php foreach ( $peerraiser['teams'] as $team ) :?>
+            <option value="<?php echo esc_attr( $team->ID ); ?>" <?php selected( absint( $peerraiser['team'] ), $team->ID ) ?>><?php echo $team->team_name; ?></option>
+        <?php endforeach; ?>
+    </select>
+</p>
