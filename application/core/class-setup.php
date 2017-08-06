ce01adbc2e55b3534c12f67d82abdc0daa82ba80
Nate Allen <email@ncallen.com>
2017-08-05 21:03:50 -0400

    Replace dev domains with real domains

--- application/core/class-setup.php
+++ application/core/class-setup.php
@@ -25,10 +25,8 @@ class Setup {
         $config->set( 'peerraiser_live_url', 'https://peerraiser.com/api/live' );
 
         $peerraiser_urls = array(
-            // 'peerraiser_url.live'    => 'https://peerraiser.com/donate/%s/%s',
-            // 'peerraiser_url.sandbox' => 'https://peerraiser.com/sandbox/%s/%s'
-            'peerraiser_url.live'    => 'http://peerraiser.dev/donate/%s/%s',
-            'peerraiser_url.sandbox' => 'http://peerraiser.dev/sandbox/%s/%s'
+            'peerraiser_url.live'    => 'https://peerraiser.com/donate/%s/%s',
+            'peerraiser_url.sandbox' => 'https://peerraiser.com/sandbox/%s/%s'
         );
         $config->import( $peerraiser_urls );
 
