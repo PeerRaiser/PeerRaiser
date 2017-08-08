cfe95380cc5cd98eccd8ea7055eb41038431d3b4
Nate Allen <email@ncallen.com>
2017-08-08 19:14:28 -0400

    Verify request when connecting plugin to peerraiser.com

--- application/controller/api/class-connection-rest-controller.php
+++ application/controller/api/class-connection-rest-controller.php
@@ -9,45 +9,72 @@ use \WP_REST_Request;
 use \WP_Error;
 
 /**
- * Activity feed controller.
+ * Connection controller.
  */
 class Connection_Rest_Controller extends WP_REST_Controller {
-    /**
-     * The base to use in the API route.
-     *
-     * @var string
-     */
-    protected $base = 'connect';
+	/**
+	 * The base to use in the API route.
+	 *
+	 * @var string
+	 */
+	protected $base = 'connect';
 
-    /**
-     * The namespace for these routes.
-     *
-     * @var string
-     */
-    protected $namespace = 'peerraiser/v1';
+	/**
+	 * The namespace for these routes.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'peerraiser/v1';
 
-    /**
-     * Register the routes for the objects of the controller.
-     */
-    public function register_routes() {
-        register_rest_route( $this->namespace, '/' . $this->base, array(
-            array(
-                'methods'              => WP_REST_Server::READABLE,
-                'callback'             => array( $this, 'confirm_connection' ),
-            ),
-            'schema' => array( $this, 'get_public_item_schema' ),
-        ) );
-    }
+	/**
+	 * Register the routes for the objects of the controller.
+	 */
+	public function register_routes() {
+		register_rest_route( $this->namespace, '/' . $this->base, array(
+			array(
+				'methods'              => 'POST, GET',
+				'callback'             => array( $this, 'confirm_connection' ),
+			),
+			'schema' => array( $this, 'get_public_item_schema' ),
+		) );
+	}
 
-    /**
-     * Get a collection of items
-     *
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_Error|WP_REST_Response
-     */
-    public function confirm_connection( $request ) {
-        $data = array( 'connected' => true );
+	/**
+	 * Confirm the connection
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function confirm_connection( $request ) {
+		$params         = $request->get_params();
+        $plugin_options = get_option( 'peerraiser_options', array() );
+		$license_key    = $plugin_options['license_key'];
+		$slug           = $params['slug'];
 
-        return new WP_REST_Response( $data, 200 );
-    }
+		if ( empty( $slug ) || empty( $license_key ) ) {
+			return new WP_REST_Response( array( 'valid' => false ), 404 );
+		}
+
+		// @todo: Change from .dev to .com
+		$response = wp_remote_post( 'http://peerraiser.dev/wp-json/heart/v1/connect', array(
+			'body' => array(
+				'slug' => $slug,
+                'license_key' => $license_key,
+			)
+		) );
+
+        if ( is_wp_error( $response ) ) {
+            error_log( print_r( $response, 1) );
+        }
+
+        $response_body = json_decode( $response['body'] );
+
+        if ( $response_body->valid === true ) {
+            update_option( 'peerraiser_slug', $slug );
+        }
+
+		$data = array( 'connected' => $response_body->valid );
+
+		return new WP_REST_Response( $data, 200 );
+	}
 }
