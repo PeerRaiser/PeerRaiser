90ba8f6fdbe02a4ff41e7aa9337905f0be7fd113
Nathan Allen <nathan@fireflypartners.com>
2017-11-12 15:49:58 -0500

    Display donation minimum, if one is set

--- views/frontend/donation-form.php
+++ views/frontend/donation-form.php
@@ -12,20 +12,22 @@
 	    <?php wp_nonce_field( 'ajax_get_fundraisers', 'get_fundraisers_nonce', false ); ?>
     </section>
 
-    <section class="peerraiser-fundraiser-selection <?php echo $peerraiser['fundraiser_select_class']; ?>"<?php if ( isset( $_GET['campaign']) && empty( $peerraiser['fundraisers'] ) ) echo ' style="display:none;"'; ?>>
-        <select name="fundraiser" id="fundraiser_select">
-            <option value=""><?php _e( 'Select a Fundraiser to Support (optional)', 'peerraiser' ); ?></option>
-            <?php foreach( $peerraiser['fundraisers'] as $fundraiser ) : ?>
-                <option value="<?php echo $fundraiser->fundraiser_slug; ?>" <?php selected( get_query_var( 'peerraiser_fundraiser' ), $fundraiser->fundraiser_slug ) ?>><?php echo $fundraiser->fundraiser_name; ?></option>
-            <?php endforeach; ?>
-        </select>
-    </section>
+    <?php if ( count( $peerraiser['fundraisers'] ) > 0 ) : ?>
+        <section class="peerraiser-fundraiser-selection <?php echo $peerraiser['fundraiser_select_class']; ?>"<?php if ( isset( $_GET['campaign']) && empty( $peerraiser['fundraisers'] ) ) echo ' style="display:none;"'; ?>>
+            <select name="fundraiser" id="fundraiser_select">
+                <option value=""><?php _e( 'Select a Fundraiser to Support (optional)', 'peerraiser' ); ?></option>
+                <?php foreach( $peerraiser['fundraisers'] as $fundraiser ) : ?>
+                    <option value="<?php echo $fundraiser->fundraiser_slug; ?>" <?php selected( get_query_var( 'peerraiser_fundraiser' ), $fundraiser->fundraiser_slug ) ?>><?php echo $fundraiser->fundraiser_name; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </section>
+    <?php endif; ?>
 
 	<section class="peerraiser-donation-amounts">
 		<label><h3><?php _e( 'Choose a Donation Amount', 'peerraiser' ); ?> </h3></label>
 		<?php // TODO: Add setting for donation amounts instead of hard coding them ?>
 		<ul class="peerraiser-donation-amount-buttons">
-			<li class="default-amounts"><input type="radio" name="donation_amount" value="25" class="peerraiser-donation-button" id="button_1"><label for="button_1"><?php echo peerraiser_money_format( 25, true, false ); ?></label>
+			<li class="default-amounts"><input type="radio" name="donation_amount" value="25" class="peerraiser-donation-button" id="button_1" checked><label for="button_1"><?php echo peerraiser_money_format( 25, true, false ); ?></label>
 			<li class="default-amounts"><input type="radio" name="donation_amount" value="50" class="peerraiser-donation-button" id="button_2"><label for="button_2"><?php echo peerraiser_money_format( 50, true, false ); ?></label>
 			<li class="default-amounts"><input type="radio" name="donation_amount" value="100" class="peerraiser-donation-button" id="button_3"><label for="button_3"><?php echo peerraiser_money_format( 100, true, false ); ?></label>
 			<li class="default-amounts"><input type="radio" name="donation_amount" value="250" class="peerraiser-donation-button" id="button_4"><label for="button_4"><?php echo peerraiser_money_format( 250, true, false ); ?></label>
@@ -36,14 +38,17 @@
 				<span class="peerraiser-currency-symbol"><?php echo $peerraiser['currency_symbol']; ?></span>
 			<?php endif; ?>
 
-
-			<?php if ( $peerraiser['currency_position'] === 'before' ) : ?>
-				<?php /* translators: 1: Currency symbol 2: Donation amount */ ?>
-				<?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), $peerraiser['currency_symbol'], '10' ); ?>
-			<?php else : ?>
-				<?php /* translators: 1: Donation amount 2: Currency symbol  */ ?>
-				<?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), '10', $peerraiser['currency_symbol'] ); ?>
-			<?php endif; ?>
+			<?php if ( $peerraiser['donation_minimum'] ) : ?>
+                <?php if ( $peerraiser['currency_position'] === 'before' ) : ?>
+                    <?php /* translators: 1: Currency symbol 2: Minimum donation amount */ ?>
+                    <?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), $peerraiser['currency_symbol'], $peerraiser['donation_minimum'] ); ?>
+			    <?php else : ?>
+                    <?php /* translators: 1: Minimum donation amount 2: Currency symbol  */ ?>
+                    <?php $field_placeholder = sprintf( __( 'Other Amount (%1$s%2$s Min)', 'peerraiser' ), $peerraiser['donation_minimum'], $peerraiser['currency_symbol'] ); ?>
+			    <?php endif; ?>
+            <?php else : ?>
+				<?php $field_placeholder = __( 'Other Amount', 'peerraiser' ); ?>
+            <?php endif; ?>
 			<input name="other_amount" class="peerraiser-donation-input" value="" maxlength="8" type="number" autocomplete="off" placeholder="<?php echo $field_placeholder; ?>" data-minimum="10" aria-label="Enter Donation Amount USD Min. $10">
 
 			<?php if ( $peerraiser['currency_position'] === 'after' ) : ?>
